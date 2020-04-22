d2728fb151668d0d17c6a2e601083b914641fd76
DiriiMQ <minhquang116@gmail.com>
2020-04-22 23:35:14 +0700

    sync

--- Python/backend/route.py
+++ Python/backend/route.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def HelloWord():
+def helloword():
     return 'Hello!'
+
 
 @app.route('/user/<name>')
 def adminpage(name):
     return 'hello %s' % name
+
 
 if __name__ == '__main__':
     app.run()
