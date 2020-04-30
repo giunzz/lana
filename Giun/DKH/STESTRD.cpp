9558d1e7768a4aabe2c3da0fb7e3141d783f51a6
DiriiMQ <minhquang116@gmail.com>
2020-04-30 13:20:52 +0700

    giuncute

--- Giun/DKH/STESTRD.cpp
+++ Giun/DKH/STESTRD.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define finame "test.inp" //file inp
+#define finame "f.inp" //file inp
 using namespace std;
 const long long maxt = 15, mint = 5, mi = 1, ma = 20; //limit
 long long d[maxt + 3], t; 
@@ -30,10 +30,11 @@ void pri(){
 int main(){
     srand((int) time(0));
     freopen(finame, "w", stdout);
-    t = Rand(mint, maxt);
-    cout << t << endl;
-    for (int i = 1; i <= t; i++){
-        pri();
+    //t = Rand(mint, maxt);
+    //cout << t << endl;
+    for (int i = 1; i <= 5; i++){
+        //pri();
+        cout << Rand(1, 100) << " ";
     }
     return 0;
 }
