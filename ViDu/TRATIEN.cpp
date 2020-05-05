83963087964303e2f33b7dfdd01d0922344219e4
giunzz <dunghoang0401237@gmail.com>
2020-05-06 01:05:05 +0700

    xynx

--- ViDu/TRATIEN.cpp
+++ ViDu/TRATIEN.cpp
@@ -1,100 +1,71 @@
-// #include<bits/stdc++.h>
+// #include <bits/stdc++.h>
 // using namespace std;
-// const string tenfile = "giun";
-// #define balabalo freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
 
-// int n , a[26] , d , ok = 1 ;
-// long long sum = 0 , s = 0;
-// vector<int> b;
+// int n, d, a[25] = {0}, s = 0, tt, check = 0, vtans = 0, ans[25] = {0};
 
-// void printf_ans()
+// void printans()
 // {
-//     for (int i = 0 ; i < (int) b.size() ; i++)
-//         cout << b[i] << " ";
-// }
-
-// void Try (int vt )
-// {
-//     if ( ok == 1 || vt == n + 1  ) return;
-//     s = 0;
-//     b.clear();
-//     for (int i = vt  ; i <= n ; i++)
+//     for (int i = 1; i <= vtans; ++i)
 //     {
-//         if ( dd[a[i]] == 0 ) s += a[i];
-//         cerr << s << " ";
-//         b.push_back(a[i]);
-//         if ( s == d  ) 
-//         {
-//             printf_ans();
-//             ok = 1;
-//         }
-//         else Try(vt + 1) ;
+//         //printf("%d ", a[ans[i]]);
+//         cout << a[ans[i]] << " ";
 //     }
 // }
 
-// int main()
+// void quaylui(int vt)
 // {
-//     balabalo;
-//     scanf("%d %d",&n,&d);
-//     for (int i = 1 ; i <= n ; i++)
-//     {
-//         scanf("%d",&a[i]);
-//         sum += a[i];
+//     if (s > d || vt == n + 1 || check) return; 
+//     if (s == d)
+//     { 
+//         printans();
+//         check = 1;
+//         return;
 //     }
-//     sort( a + 1 , a + 1 + n );
-//     if ( sum == d ) 
-//         for (int i = 1 ; i <= n ; i++) cout << a[i] << " ";
-//     else if ( sum < d ) cout << -1;
-//          else  Try(1);
+//     if (vt == 1) tt = 1; 
+//     else tt = ans[vtans] + 1; 
+//     for (int i = tt; i <= n; i++)
+//     {
+//         s += a[i], ans[++vtans] = i;
+//         quaylui(vt + 1);
+//         s -= a[i];
+//         --vtans;
+//     }
+// }
+
+// int main(){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     freopen("tratien.inp", "r", stdin);
+//     freopen("tratien.out", "w", stdout);
+//     //scanf("%d %d", &n, &d);
+//     cin >> n >> d;
+//     for (int i = 1; i <= n; i++) {
+//         //scanf("%d", &a[i]);
+//         cin >> a[i]; 
+//         s += a[i];
+//     }
+//     if (s < d) {
+//         //printf("-1");
+//         cout << -1; 
+//         return 0;
+//     } 
+//     sort(a + 1, a + 1 + n);
+//     if (s == d)
+//     {
+//         for(int i = 1; i <= n; i++) cout << a[i] << " "; //printf("%d", a[i]);
+//         return 0;
+//     }
+//     s = 0; 
+//     quaylui(1);
+//     if(!check) cout << -1; // printf("-1");
 //     return 0;
 // }
 
 #include <bits/stdc++.h>
-#define ll long long
-#define elif esle if
 using namespace std;
 
-ll n, d, a[25] = {0}, s = 0, tt, check = 0;
-vector<ll> ans;
-
-void printans(){
-    for (int i = 0; i < ans.size(); ++i){
-        printf("%I64d ", a[ans[i]]);
-    }
-}
-
-void quaylui(ll vt){
-    if (s > d || vt == n + 1 || check) return; //tu nghi
-    if (s == d){ //tu nghi
-        printans();
-        check = 1;
-        return;
-    }
-    if (vt == 1) tt = 1; //neu ans chua co phan tu thi bat dau chon tu vi tri dau tien
-    else tt = ans.back() + 1; //nguoc lai tim tu vi tri tiep theo cua to tien cuoi cung da chon
-    for (ll i = tt; i <= n; i++){
-        s += a[i]; //tu nghi het
-        ans.push_back(i);
-        quaylui(vt + 1);
-        s -= a[i];
-        ans.pop_back();
-    }
-}
-
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    freopen("giun.inp", "r", stdin);
-    freopen("giun.out", "w", stdout);
-    scanf("%I64d %I64d", &n, &d);
-    for (int i = 1; i <= n; i++) scanf("%I64d", &a[i]), s += a[i];
-    if (s < d) {cout << -1; return 0;} //tong a1 -> an < d
-    sort(a + 1, a + 1 + n);
-    if (s == d){
-        for(int i = 1; i <= n; i++) printf("%I64d", &a[i]);
-        return 0;
-    }
-    s = 0; //bat dau tim nghiem s la tong cac so duoc chon trong vector ans
-    quaylui(1);
-    if(!check) printf("-1");
+    freopen("tratien.inp", "r", stdin);
+    freopen("tratien.out", "w", stdout);
+    cout << 1;
 }
--- ViDu/tt.cpp
+++ ViDu/tt.cpp
@@ -1,126 +1,59 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-const int maxn = 1e6 + 7;
-#define balabolo freopen("giun.inp","r",stdin) , freopen("giun.out","w",stdout);
-#define giuncute ios_base::sync_with_stdio(0) , cin.tie(0);
-int n , a[maxn] ,  fen[maxn];
-
-void update(int p, int val)
-{
-    for(int i = p; i <= n; i += i & -i)
-        fen[i] += val;
-}
-
-int sum(int p) 
-{
-    int ans = 0;
-    for(int i = p; i; i -= i & -i)
-        ans += fen[i];
-    return ans;
-}
-
-int main()
-{
-    balabolo;
-    giuncute;
-    cin >> n ; 
-    for (int i = 1 ; i <= n ; i++) 
-    {   cin >> a[i] ;
-        update(i , a[i]);
-    }
-    for (int i = 1 ; i <= n ; i++) cerr << fen[i] << " ";
-    cerr << endl;
-    cout << sum(6);
-
-/*
-/*long long  sa = 0 , sb = 0 ;
-
-
-void fastscan(int &num) 
-{  
-    bool neg = false; 
-    register int c;
-    num = 0;  
-    c = getchar(); 
-    if (c == ' ' || c == '\n') fastscan(num);
-    if (c=='-') 
-    {  
-        neg = true;  
-        c = getchar(); 
-    }  
-    for (; (c>47 && c<58); c=getchar()) 
-        num = num *10 + c - 48; 
-    if (neg) 
-        num *= -1; 
-} 
-
-int main()
-{
-    balabolo(file);
-    fastscan(n) ; fastscan(t);
-    for (int i = 1 ; i <= n ; i++)
-    {
-        fastscan(a[i]) ; fastscan(b[i]) ;
-        sa += a[i];
-        sb += b[i];
-    }
-    for (int i = 0 ; i <= t ; i++) 
-    {
-        printf("%I64d\n", sa - i * sb);
-    }
-    return 0;
-}*/
-
-}
 #include <bits/stdc++.h>
 using namespace std;
-const string file = "giun";
-const int maxn = 1e7+7;
-#define balabolo(a) freopen((a + ".inp").c_str(), "r", stdin) ,freopen((a + ".out").c_str(), "w", stdout)
-long n , t , dd[maxn]={0}  ;
-void fastscan(long &num) 
-{  
-    bool neg = false; 
-    register int c;
-    num = 0;  
-    c = getchar(); 
-    if (c == ' ' || c == '\n') fastscan(num);
-    if (c=='-') 
-    {  
-        neg = true;  
-        c = getchar(); 
-    }  
-    for (; (c>47 && c<58); c=getchar()) 
-        num = num *10 + c - 48; 
-    if (neg) 
-        num *= -1; 
-} 
 
-int main()
+int n, d, a[25] = {0}, s = 0, tt, check = 0;
+vector<int> ans;
+
+void printans()
 {
-  //balabolo(file);
-  fastscan(n) ; fastscan(t);
-  long long bayhoi=0,ans=0;
-  for (int i = 1 ; i <= n ; i++) 
-  {
-  	long a,b;
-  	fastscan(a) ; 
-    fastscan(b);
-  	bayhoi+=b; 
-    ans+=a;
-  	dd[a/b+1]+=(b-(a%b)); 
-    dd[a/b+2]+=(a%b);
-  }
-  printf("%I64d\n",ans);
-  for (int i = 1 ; i <= t ; i++) 
-  {
-  	bayhoi-=dd[i];
-  	ans-=bayhoi;
-  	printf("%I64d\n",ans);
-  }
-  return 0;
+    for (int i = 0; i < (int) ans.size(); ++i)
+    {
+        printf("%d ", a[ans[i]]);
+    }
 }
 
+void quaylui(int vt)
+{
+    if (s > d || vt == n + 1 || check) return; 
+    if (s == d)
+    { 
+        printans();
+        check = 1;
+        return;
+    }
+    if (vt == 1) tt = 1; 
+    else tt = ans.back() + 1; 
+    for (int i = tt; i <= n; i++)
+    {
+        s += a[i]; 
+        ans.push_back(i);
+        quaylui(vt + 1);
+        s -= a[i];
+        ans.pop_back();
+    }
+}
 
-
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("tratien.inp", "r", stdin);
+    freopen("tratien.out", "w", stdout);
+    scanf("%d %d", &n, &d);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]); 
+        s += a[i];
+    }
+    if (s < d) {
+        printf("-1"); 
+        return 0;
+    } 
+    sort(a + 1, a + 1 + n);
+    if (s == d)
+    {
+        for(int i = 1; i <= n; i++) printf("%d", a[i]);
+        return 0;
+    }
+    s = 0; 
+    quaylui(1);
+    if(!check) printf("-1");
+}
