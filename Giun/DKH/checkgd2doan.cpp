ad93fc1cc1a41c09a344577a71d45b27f3e0d7e6
giunzz <dunghoang0401237@gmail.com>
2020-05-21 12:16:52 +0700

    sync

--- Giun/DKH/checkgd2doan.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-pair<int, int> a, b, a1, b1;
-
-int main(){
-    cin >> a.first >> a.second >> b.first >> b.second >> a1.first >> a1.second >> b1.first >> b1.second;
-    //tu code tiep
-}
