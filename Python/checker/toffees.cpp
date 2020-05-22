eaba808045f3467b2f1265d9778f53a58fbdc5b8
giunzz <dunghoang0401237@gmail.com>
2020-05-22 18:21:26 +0700

    sync

---
+++ Python/checker/toffees.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ii int
+#define ll long long
+#define cii const int
+#define cll const long long
+#define opt ios_base::sync_with_stdio(0); cin.tie(0)
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define lpdk(a, b, c, d) for(ll a = b; c; d) 
+#define pp(a, b) pair<a, b>
+#define vec(a) vector<a>
+#define vecite(a) vector<a>::iterator
+#define fi first
+#define se second
+using namespace std;
+cll MOD = 1e9 + 7;
+const string tenfile = "toffees";
+#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+
+cll maxn = 1e6 + 7;
+ll n, ans[maxn] = {0};
+string s;
+
+int main(){
+    opt;
+    file;
+    cin >> n >> s;
+    s = "0" + s;
+    ans[1] = 1;
+    lp(i, 1, n - 1){
+        if(s[i] == '=') ans[i + 1] = ans[i];
+        else if(s[i] == 'R') ans[i + 1] = ans[i] + 1;
+        else if(s[i] == 'L' && ans[i] > 1) ans[i + 1] = 1;
+        else ans[i + 1] = ans[i] - 1;
+    }
+    ii i = n - 1, dem = 0;
+    while((s[i] == 'R' || s[i] == '=') && i >= 0)
+        if(s[i--] == 'R') ++dem;
+    if(i > 0) ans[n] += dem;
+    //lp(i, 1, n) cerr << ans[i] << " ";
+    lpd(i, n, 2){
+        if(s[i - 1] == '=') ans[i - 1] = ans[i];
+        else if(s[i - 1] == 'R' && ans[i] < ans[i  -1]) ans[i - 1] = ans[i] + 1;
+        else if(s[i - 1] == 'L' && ans[i] < 1) ans[i] = 1, ans[i - 1] = 2;
+        else if(s[i - 1] == 'L' && ans[i] > ans[i  - 1]) ans[i - 1] = ans[i] + 1;
+    }
+    lp(i, 1, n) cout << ans[i] << " ";
+}
