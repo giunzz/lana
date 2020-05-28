04b18e0df912f4062409aa1ae37469cb12a5f06b
giunzz <dunghoang0401237@gmail.com>
2020-05-28 15:53:24 +0700

    sync

--- Python/checker/toffees.cpp
+++ Python/checker/toffees.cpp
@@ -1,48 +1,77 @@
 #include <bits/stdc++.h>
-#define ii int
+
+using namespace std;
+
+#define fileInput(problemName) freopen ((string(problemName) + ".inp").c_str(), "r", stdin);freopen ((string(problemName) + ".out").c_str(), "w", stdout);
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define ii pair < int, int >
 #define ll long long
-#define cii const int
-#define cll const long long
-#define opt ios_base::sync_with_stdio(0); cin.tie(0)
-#define lp(a, b, c) for(ll a = b; a <= c; a++)
-#define lpd(a, b, c) for(ll a = b; a >= c; a--)
-#define lpdk(a, b, c, d) for(ll a = b; c; d) 
-#define pp(a, b) pair<a, b>
-#define vec(a) vector<a>
-#define vecite(a) vector<a>::iterator
 #define fi first
 #define se second
-using namespace std;
-cll MOD = 1e9 + 7;
-const string tenfile = "toffees";
-#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
 
-cll maxn = 1e6 + 7;
-ll n, ans[maxn] = {0};
-string s;
+const int inf = 1e9 + 7;
+const int esf = 1e-9;
+const int N = 1e6 + 7;
 
-int main(){
-    opt;
-    file;
-    cin >> n >> s;
-    s = "0" + s;
-    ans[1] = 1;
-    lp(i, 1, n - 1){
-        if(s[i] == '=') ans[i + 1] = ans[i];
-        else if(s[i] == 'R') ans[i + 1] = ans[i] + 1;
-        else if(s[i] == 'L' && ans[i] > 1) ans[i + 1] = 1;
-        else ans[i + 1] = ans[i] - 1;
+int n;
+int a[N];
+char q[N];
+
+int main() {
+    fileInput("toffees");
+    fast;
+    cin >> n;
+    for (int i = 1; i <= n - 1; i++) {
+        cin >> q[i];
     }
-    ii i = n - 1, dem = 0;
-    while((s[i] == 'R' || s[i] == '=') && i >= 0)
-        if(s[i--] == 'R') ++dem;
-    if(i > 0) ans[n] += dem;
-    //lp(i, 1, n) cerr << ans[i] << " ";
-    lpd(i, n, 2){
-        if(s[i - 1] == '=') ans[i - 1] = ans[i];
-        else if(s[i - 1] == 'R' && ans[i] < ans[i  -1]) ans[i - 1] = ans[i] + 1;
-        else if(s[i - 1] == 'L' && ans[i] < 1) ans[i] = 1, ans[i - 1] = 2;
-        else if(s[i - 1] == 'L' && ans[i] > ans[i  - 1]) ans[i - 1] = ans[i] + 1;
+
+    memset(a, 0, sizeof(a));
+
+    for (int i = 1; i <= n - 1; i++) {
+        if (q[i] == 'L') {
+            a[i] = max(a[i + 1] + 1, a[i] + 1);
+        } else if (q[i] == 'R') {
+            a[i + 1] = max(a[i] + 1, a[i + 1] + 1);
+        } else if (q[i] == '=') {
+            a[i] += 1;
+            a[i + 1] += 1;
+        }
     }
-    lp(i, 1, n) cout << ans[i] << " ";
+
+    for (int i = n - 1; i >= 1; i--) {
+        if (q[i] == 'L') { /*a[i] > a[i + 1]*/
+            if (a[i + 1] == 0 and a[i] > a[i + 1] + 1) {
+                a[i + 1] += 1;
+            } else if (a[i + 1] == 0 and a[i] == a[i + 1] + 1) {
+                a[i + 1] += 1;
+                a[i] += 1;
+            } else if (a[i + 1] != 0 and a[i] == a[i + 1]) {
+                a[i] += 1;
+            } else continue;
+        } else if (q[i] == 'R') { /*a[i] < a[i + 1]*/
+            if (a[i] == 0 and a[i + 1] > a[i] + 1) {
+                a[i] += 1;
+            } else if (a[i] == 0 and a[i + 1] == a[i] + 1) {
+                a[i] += 1;
+                a[i + 1] += 1;
+            } else if (a[i] != 0 and a[i + 1] == a[i]) {
+                a[i + 1] += 1;
+            } else continue;
+        } else if (q[i] == '=') { /*a[i] == a[i + 1]*/
+            if (a[i] == a[i + 1]) {
+                continue;
+            } else if (a[i] > a[i + 1]) {
+                a[i + 1] += (a[i] - a[i + 1]);
+            } else if (a[i] < a[i + 1]) {
+                a[i] += (a[i + 1] - a[i]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    return (0);
 }
