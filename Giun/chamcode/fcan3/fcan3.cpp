c01c14d06a53fbbadaa1c78a3b14c273795925c4
giunzz <dunghoang0401237@gmail.com>
2020-06-09 12:31:07 +0700

    huhuu éo đc

--- Giun/chamcode/fcan3/fcan3.cpp
+++ Giun/chamcode/fcan3/fcan3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define giuncute ios_base::sync_with_stdio(0) , cin.tie(0)
 const string tenfile = "fcan3";
 #define balabalo freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
-
-int m , dem = 0  , d[15] = {0};
+#define linh long long 
+long long m , dem = 0  , d[300] = {0};
 
 void print_ans()
 {
@@ -14,13 +14,13 @@ void print_ans()
     for (int i = 0 ; i <= dem ; i++)
     {
         if (d[i] != 0 ) 
-            for (int j = 1 ; j <= d[i] ; j++) cout << (long long)pow(3,i) << " ";
+            for (int j = 1 ; j <= d[i] ; j++) cout << (linh)pow(3,i) << " ";
     }
 }
 
 void chonn( int chon)
 {
-    if ( pow(3, chon) > m ) {print_ans();}
+    if ( (linh ) pow(3, chon) > m ) {print_ans();}
     else 
     {
         if (chon == 0) d[0] = m;
