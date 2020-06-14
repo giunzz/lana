f0d00997087479d8104b5bdf70f3ed1254eec072
giunzz <dunghoang0401237@gmail.com>
2020-06-14 15:27:33 +0700

    èlewf

--- ViDu/MATM.cpp
+++ ViDu/MATM.cpp
@@ -1,5 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define giuncute ios_base::sync_with_stdio(0) , cin.tie(0)
 const string tenfile = "giun";
 #define balabalo freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+
+int n , k ,  a[25] , b[25] , ans = 0 , x[25] , sum = 0 ;
+
+void fastscan(int &number) 
+{ 
+    bool negative = false; 
+    register int c; 
+    number = 0; 
+    c = getchar(); 
+    if (c == ' ') fastscan(number);
+    if (c=='-') 
+    { 
+        negative = true; 
+        c = getchar(); 
+    } 
+    for (; (c>47 && c<58); c=getchar()) 
+        number = number *10 + c - 48; 
+    if (negative) 
+        number *= -1; 
+}
+
+void check_ans()
+{
+    for (int i = 1 ; i <= n ; i++) sum += b[i]*a[i];
+    cerr << sum << " ";
+    if ( sum == k ) ans ++;
+    sum = 0;
+}
+
+void chon ( int vt)
+{
+    if ( vt == n + 1) check_ans();
+    else 
+    {
+        for (int i = 1 ; i <= n ; i++)
+        {
+            x[i] = b[i];
+            chon (vt + 1);
+        }
+    }
+}
+
+int main()
+{
+    balabalo;
+    fastscan(n) , fastscan(k);
+    for (int i = 1 ; i <= n ; i++) fastscan(a[i]);
+    for (int i = 1 ; i <= n ; i++) fastscan(b[i]);
+    chon(1);
+    cout << ans;
+}
