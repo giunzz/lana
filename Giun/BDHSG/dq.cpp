0aae81159dc8b744949af30eae4cdc54cec5ea06
giunzz <dunghoang0401237@gmail.com>
2020-06-29 08:10:00 +0700

    sync

---
+++ Giun/BDHSG/dq.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ii int
+#define ll long long
+#define cii const int
+#define cll const long long
+#define opt ios_base::sync_with_stdio(0); cin.tie(0)
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define pp(a, b) pair<a, b>
+#define vec(a) vector<a>
+#define vecite(a) vector<a>::iterator
+#define fi first
+#define se second
+#define mp(a, b) map<a, b>
+#define st(obj) system((obj).c_str());
+using namespace std;
+cll MOD = 1e9 + 7;
+const double esf = 1e-9;
+const string tenfile = "f";
+#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+
+cll maxn = 1e4 + 7;
+struct stc{
+    ll a, b, c;
+}d[maxn];
+ll f[maxn] = {0}, mend = INT_MIN, bg, n;
+
+ii main(){
+    opt;
+    file;
+    cin >> n;
+    lp(i, 1, n){
+        cin >> d[i].a >> d[i].b >> d[i].c;
+        mend = max(d[i].b, mend);
+    }
+    lp(i, 1, mend){
+        f[i] = f[i - 1];
+        lp(j, 1, n){
+            if(i == d[j].b){
+                bg = d[j].a;
+                f[i] = max(f[i], f[bg] + d[j].c);
+            }
+        }
+    }
+    cout << f[mend];
+}
