2a6174e92ef13a58f3825a29b4e01a0b7d06ef0d
giunzz <dunghoang0401237@gmail.com>
2020-07-01 18:15:36 +0700

    sync

---
+++ Giun/BDHSG/ticket.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ii int
+#define ll long long
+#define cii const int
+#define cll const long long
+#define opt ios_base::sync_with_stdio(0); cin.tie(0)
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define pp(a, b) pair<a, b>
+#define vec(a) vector<a>
+#define vecite(a) vector<a>::iterator
+#define fi first
+#define se second
+#define mp(a, b) map<a, b>
+#define st(obj) system((obj).c_str());
+using namespace std;
+cll MOD = 1e9 + 7;
+const double esf = 1e-9;
+const string tenfile = "f";
+#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+
+ll cnt[4], n, num;
+
+ii main(){
+    opt;
+    file;
+    cin >> n;
+    lp(i, 1, n){
+        cin >> num;
+        ++cnt[num % 3];
+    }
+    cout << cnt[0] / 2 + min(d[1], d[2]);
+}
