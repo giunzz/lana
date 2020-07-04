5af2d7302588b132f6cb5818262c651e6fd41351
giunzz <dunghoang0401237@gmail.com>
2020-07-04 16:09:50 +0700

    sync

--- code_thi/bai22.cpp
+++ code_thi/bai22.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long  n , a[1000],b[1000];
+long long  n ,p2[65]={1};
+
+long long dequy(long long vt)
+{
+    if ( vt == 1 ) return 0;
+    long long lap;
+    for (int i = 0 ; i <= 63 ; i++)
+    {
+        if (p2[i] >= vt)
+        {
+            lap = i ;
+            break;
+        }
+    }
+    return ((dequy(vt - p2[lap] / 2) + 1) % 3);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    freopen("giun.inp","r", stdin);freopen("giun.out","w",stdout);
-   // freopen("bai22.inp","r", stdin);freopen("bai22.out","w",stdout);
-    cin >> n;
-    a[0] = 0 , a[1] = 1;
-    b[0] = 0;
-    for (int i = 1 ; i <= 10 ; i++)
-    {
-        b[i] = (a[i] + 1 ) % 3;
-
-    }
-    cout << a[n];
+    //freopen("giun.inp","r", stdin);freopen("giun.out","w",stdout);
+    freopen("bai22.inp","r", stdin);freopen("bai22.out","w",stdout);
+    for (int i = 1 ; i <= 65 ; i++) p2[i] =  p2[i-1] * 2;
+    while ( cin >> n ) cout << dequy(n) << endl;
     return 0;
 }
--- code_thi/test1.cpp
+++ code_thi/test1.cpp
@@ -1,24 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ii int
+#define ll long long
+#define cii const int
+#define cll const long long
+#define opt ios_base::sync_with_stdio(0); cin.tie(0)
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define pp(a, b) pair<a, b>
+#define vec(a) vector<a>
+#define vecite(a) vector<a>::iterator
+#define fi first
+#define se second
+#define mp(a, b) map<a, b>
+#define filln(a, b) memset(a, b, sizeof(a))
+#define st(obj) system((obj).c_str());
 using namespace std;
-int n , t , a[1008];
-
-int UCLN (int x , int y )
-{
-    int  t;
-    while (y != 0 )
-    {
-        t = x % y;
-        x = y;
-        y = t;
+cll MOD = 1e9 + 7;
+const double esf = 1e-9;
+const string tenfile = "giun";
+#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+ll p2[65] = {1}, n, t;
+ll dequy(ll vt){
+    if(vt == 1) return 0;
+    ll lap;
+    lp(i, 0, 63){
+        if(p2[i] >= vt) {
+            lap = i;
+            break;
+        }
     }
-    return x;
+    return ((dequy(vt - p2[lap] / 2) + 1) % 3);
 }
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout << UCLN(0,3) << endl;
-    cout << 1000000000 / 2 << endl;
-    cout << log(8) <<" " << log2(8);
-
+ii main(){
+    opt;
+    file;
+    lp(i, 1, 64) p2[i] = p2[i - 1] * 2;
+    while(cin >> n){
+        cout << dequy(n) << endl;
+    }
 }
