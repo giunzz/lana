1e30532f16116843798b955cd339bf2e4870a1ed
giunzz <dunghoang0401237@gmail.com>
2020-07-06 08:32:15 +0700

    sync

--- code_thi/PSNGUYEN.cpp
+++ code_thi/PSNGUYEN.cpp
@@ -2,13 +2,46 @@
 using namespace std;
 #define ll long long 
 ll m , n , ans = 0 , b , t, mau;
+
+
+void fastscan(ll &number) 
+{ 
+    //variable to indicate sign of input number 
+    bool negative = false; 
+    register int c; 
+  
+    number = 0; 
+  
+    // extract current character from buffer 
+    c = getchar(); 
+    if (c=='-') 
+    { 
+        // number is negative 
+        negative = true; 
+  
+        // extract the next character from the buffer 
+        c = getchar(); 
+    } 
+  
+    // Keep on extracting characters if they are integers 
+    // i.e ASCII Value lies from '0'(48) to '9' (57) 
+    for (; (c>47 && c<58); c=getchar()) 
+        number = number *10 + c - 48; 
+  
+    // if scanned input has a negative sign, negate the 
+    // value of the input number 
+    if (negative) 
+        number *= -1; 
+} 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     freopen("PSNGUYEN.inp","r",stdin);freopen("PSNGUYEN.out","w",stdout);
     //freopen("giun.inp","r",stdin);freopen("giun.out","w",stdout);
-    cin >> m >> n ;
+    fastscan(m);
+    fastscan(n);
+    //cerr << m << " " << n;
     for (int i = 1 ; i < n ; i++)
     {
         mau = n + i;
