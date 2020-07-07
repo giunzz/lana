2bae1da1e002a892259b4512cb3de519d6dbfe48
giunzz <dunghoang0401237@gmail.com>
2020-07-07 08:44:02 +0700

    sync

--- Giun/BDHSG/tamgiacvuongbk.cpp
+++ Giun/BDHSG/tamgiacvuongbk.cpp
@@ -23,7 +23,11 @@ cll maxn = 15e2 + 7;
 struct point{
     ll x, y;
 }p[maxn];
-ll n, ans = 0; 
+struct line{
+    ll a, b, c;
+};
+ll n, ans = 0;
+point p[maxn]; 
 
 bool operator<(point const &l, point const &r){
     return (l.x < r.x) || ((l.x == r.x) && (l.y < r.y));
@@ -33,7 +37,8 @@ bool operator==(point const &l, point const &r){
     return (l.x == r.x && l.y == r.y);
 }
 
-map<point, ll> cntvt;
+vec(line) aldt;
+map<line, ll> cntp;
 
 void xl(){
     sort(p + 1, p + 1 + n);
@@ -47,19 +52,57 @@ void xl(){
     }
 }
 
+void getPT(point p1, point p2, point pr, point pt){
+    point cp = {p2.x - p1.x, p2.y - p1.y};
+    pt = {-cp.y, cp.x};
+    ll r = __gcd(abs(pt.x), abs(pt.y));
+    if(!r) r = 1;
+    pt.x /= r, pt.y /= r;
+    if(pt.x < 0) pt.x *= -1, pt.y *= -1;
+    pr = p1;
+}
+
+void getabc(line d, point pt, point pr){
+    d.a = pt.x, d.b = pt.y; d.c = -(pt.x*pr.x + pt.y*pr.y);
+    ll r = __gcd(abs(d.a), __gcd(abs(d.b), abs(d.c)));
+    if(!r) r = 1;
+    d.a /= r, d.b /= r, d.c /= r;
+}
+
+void xld(line d){
+    if(d.a < 0) d.a *= -1, d.b *= -1, d.c *= -1;
+}
+
+bool check(line d, point pnt){
+    return !(d.a * pnt.x + d.b * pnt.y + d.c);
+}
+
 void prc(){
-    ll tmp;
-    point u, v;
+    map<line, ll> cntdt;
+    line tmp;
+    point p1, p2, pr, pt;
     lp(i, 1, n){
         lp(j, i + 1, n){ //moi vector u = vt ij tim cac diem tao voi diem i la vector phap tuyen cua vt ij
-            u.x = p[j].x - p[i].x;
-            u.y = p[j].y - p[j].y;
-            tmp = __gcd(abs(u.x), abs(u.y));
-            if(!tmp) tmp = 1;
-            u.x /= tmp, u.y /= tmp;
-            if(u.x < 0) u.x *= -1, u.y *= -1;
-            ++cnt[vt];
+            p1 = p[i], p2 = p[j];
+            getPT(&p1, &p2, &pr, &pt);
+            getabc(&tmp, &pt, &pr);
+            xld(&tmp);
+            if(!cntdt[tmp]++) aldt.push_back(tmp);
         }   
+    }
+    lp(i, 1, n){
+        lp(j, 0, aldt.size() - 1){
+            if(check(aldt[j], p[i])) ++cntp[aldt[j]];
+        }
+    }
+}
+
+void cnttg(){
+    point u, v;
+    lp(i, 1, n){
+        lp(j, i + 1, n){
+
+        }
     }
 }
 
@@ -70,6 +113,7 @@ ii main(){
     lp(i, 1, n){
         cin >> p[i].x >> p[i].y;
     }
-    xl();
+    //xl();
     prc();
+    cnttg();
 }
