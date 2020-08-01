8880f32af0e03e4940bc79cd3dec3c3d8c7d436e
DiriiMQ <minhquang116@gmail.com>
2020-08-01 11:30:58 +0700

    sync

--- Giun/BDHSG/test.cpp
+++ Giun/BDHSG/test.cpp
@@ -1,27 +1,32 @@
-#include <bits/stdc++.h>
-#define ii int
-#define ll long long
-#define cii const int
-#define cll const long long
-#define opt ios_base::sync_with_stdio(0); cin.tie(0)
-#define lp(a, b, c) for(ll a = b; a <= c; a++)
-#define lpd(a, b, c) for(ll a = b; a >= c; a--)
-#define pp(a, b) pair<a, b>
-#define vec(a) vector<a>
-#define vecite(a) vector<a>::iterator
-#define fi first
-#define se second
-#define mp(a, b) map<a, b>
-#define setE(a, b) fill_n(a, sizeof(a)/sizeof(a[0]), b)
-#define st(a) system((a).c_str());
-using namespace std;
-cll MOD = 1e9 + 7;
-const double esf = 1e-9;
-const string tenfile = "f";
-#define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-ii main(){
-    opt;
-    // file;
-    cout << ((15) ^ (1 << 2));
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    string S, tmp;
+    getline(cin, S);
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+    vector<string>ans;
+    for(char&c:S){
+        if(c==' ') {
+            ans.push_back(tmp);
+            tmp.clear();
+        }
+        else{
+            tmp.push_back(c);
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    for(string&x:ans)cout<<x<<" ";
 }
