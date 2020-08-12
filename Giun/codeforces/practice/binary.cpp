8581bcaf3e6401104ed6b0470f853db42a8521b9
DiriiMQ <minhquang116@gmail.com>
2020-08-13 00:34:42 +0700

    sync

--- Giun/codeforces/practice/binary.cpp
+++ Giun/codeforces/practice/binary.cpp
@@ -21,12 +21,40 @@ const string tenfile = "f";
 #define file freopen((tenfile + ".inp").c_str(), "r", stdin); freopen((tenfile + ".out").c_str(), "w", stdout)
 
 cll maxd = 36;
-ll c[maxd][maxd] = {0}; //Cnk
+ll c[maxd][maxd] = {{0}}, n, k, ans, d0, d00; //Cnk
+vec(ll) a;
+
+ll getS(ll q, ll r){
+    ll tmp = 0;
+    lp(i, 0, q && (r-i>=0)) tmp += c[i][r - i];
+    return tmp;
+}
 
 ii main(){
     opt;
-    file;
+    // file;
     lp(i, 0, 35) c[i][0] = 1;
     lp(i, 1, 35) lp(j, 1, i) c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
-    
+    while(cin >> n >> k){
+        a.clear(); ans = 0, d0 = 0, d00 = 0;
+        while(n) a.push_back(n&1), d00 += (n&1)?0:1, n >>= 1;
+        if(!k) {ans = a.size() - 1; if(!d00) ++ans; cout << ans << endl; continue;}
+        reverse(a.begin(), a.end());
+        lp(i, 1, a.size() - 1)
+            if(a[i]) ans += getS(a.size() - i - 2, k - d0 - 1)  + ((d00 + 1) == k)?1:0;
+            else ++d0;
+        lp(i, 0, a.size() - 2) ans += c[i][k];
+        if(d00==k)++ans;
+        if(k==1)++ans;
+        cout << ans << endl;
+    }
 }
+
+// https://codeforces.com/group/FLVn1Sc504/contest/234046/problem/A
+// https://codeforces.com/group/FLVn1Sc504/contest/266446/problem/B
+// https://codeforces.com/group/FLVn1Sc504/contest/274811/problem/J
+// https://codeforces.com/contest/1393/problem/D
+// https://codeforces.com/group/FLVn1Sc504/contest/274425/problem/P
+// http://vnoi.info/wiki/translate/topcoder/dynamic-programming
+// http://vnoi.info/wiki/algo/dp/basic-problems
+// http://vnoi.info/wiki/algo/dp/palindrome-problems
