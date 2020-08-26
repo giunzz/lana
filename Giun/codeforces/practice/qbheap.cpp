1261383aee7e6d98dc8371d33358876e699f7484
DiriiMQ <minhquang116@gmail.com>
2020-08-27 01:44:08 +0700

    sync

--- Giun/codeforces/practice/qbheap.cpp
+++ Giun/codeforces/practice/qbheap.cpp
@@ -4,9 +4,6 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    // freopen("f.inp", "r", stdin);
-    // freopen("f.out", "w", stdout);
-    // set<int, greater<int>> a;
     priority_queue<int> a;
     int x;
     char stt;
