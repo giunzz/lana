0dbaacb69ba8130da5105b98700fcf3f76190a79
DiriiMQ <minhquang116@gmail.com>
2020-08-31 14:16:45 +0700

    sync

--- Giun/BDHSG/fullbox.cpp
+++ Giun/BDHSG/fullbox.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const long long maxbit = 62;
+
 int dn[68], cnt[68], ans[68];
 void nhiphan(long long n)
 {
-	vitri.clear();
 	memset(dn ,0, sizeof(dn));
 	vector<int> np;
 	while(n){
 		np.push_back(n&1);
 		n >>= 1;
 	}
-	reverse(np.begin(), np.end());
-	for (int i = 0; i < i.size(); i++) {dn[i] = np[i];}
+	for (int i = 0; i < np.size(); i++) {dn[i] = np[i];}
 }
 
 const int maxm = 1e5 + 7;
@@ -22,8 +22,8 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	freopen("fullbox.inp","r",stdin);
-	freopen("fullbox.out","w",stdout);
+	freopen("f.inp","r",stdin);
+	freopen("f.out","w",stdout);
 	int t;
 	cin>>t;
 	long long n, m;
@@ -36,9 +36,9 @@ int main()
 		for(int i = 1; i <= m; i++){
 			++cnt[__builtin_ctz(a[i])];
 		}
-		for(int i = 0; i <= 64; i++){
+		for(int i = 0; i <= 62; i++){
 			if(dn[i]){
-				for(j = i; j >= 0; j--){
+				for(int j = i; j >= 0; j--){
 					if(cnt[j] )
 				}
 			}
--- Giun/BDHSG/test.cpp
+++ Giun/BDHSG/test.cpp
@@ -4,27 +4,6 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    // freopen("f.inp", "r", stdin);
-    // freopen("f.out", "w", stdout);
-    // set<int, greater<int>> a;
-    priority_queue<int> a;
-    int x;
-    char stt;
-    while(cin >> stt){
-        if(stt == '+' && cin >> x && a.size() < 15000) a.push(x);
-        else if(stt == '-' && a.size()) {
-            int tmp = a.top();
-            while(a.size() && tmp == a.top()) {a.pop();}
-        }
-    }
-    vector<int> b;
-    while(a.size()){
-        b.push_back(a.top());
-        int tmp = a.top();
-        while(a.size() && tmp == a.top()) a.pop();
-    }
-    cout << b.size() << endl;
-    for(auto i : b) {
-        cout << i << endl;
-    }
+    long long a = (long long)1 << (long long)3;
+    cout << a;
 }
