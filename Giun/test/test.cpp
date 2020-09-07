5b1b6d10726b099a8258da3465a16aa551a619f4
DiriiMQ <minhquang116@gmail.com>
2020-09-07 20:50:10 +0700

    sync

--- Giun/test/test.cpp
+++ Giun/test/test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    long long n;
-    cin >> n;
-    cout << n;
+    long long n(100), a(200), b(300);
+    n = a = b = 150;
+    cout << n << ' ' << a << ' ' << b;
 }
