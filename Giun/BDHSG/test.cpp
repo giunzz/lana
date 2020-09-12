a863f549fcd628c16188d1dc9ab6722b1375723e
giunzz <dunghoang0401237@gmail.com>
2020-09-12 21:04:20 +0700

    sync

--- Giun/BDHSG/test.cpp
+++ Giun/BDHSG/test.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
+
+ll a, n, b = 0;
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    long long a = (long long)1 << (long long)3;
-    cout << a;
+    cin >> a >> n;
+    ll i = 0;
+    while(LLONG_MAX/n >= i){
+        b = max(b, a ^ (n * i++));
+    }
+    cout << b;
 }
