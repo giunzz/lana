b2dd362c6fbf6164afc27d2af02278c3f8d21eae
DiriiMQ <minhquang116@gmail.com>
2020-09-15 19:27:42 +0700

    sync

--- Giun/BDHSG/test.cpp
+++ Giun/BDHSG/test.cpp
@@ -2,15 +2,20 @@
 #define ll long long
 using namespace std;
 
-ll a, n, b = 0;
+ll a, n, b = LLONG_MAX;
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> a >> n;
     ll i = 0;
-    while(LLONG_MAX/n >= i){
-        b = max(b, a ^ (n * i++));
-    }
-    cout << b;
+    // while(1e18/n >= i){
+    //     b = min(b, a ^ (n * i++));
+    //     cerr << 1;
+    // }
+    ll tmp = 1e18/n * n;
+    // cerr << (a ^ 0);
+    cout << (a ^ tmp);
+    b = a ^ tmp;
+    cerr << endl << tmp;
 }
