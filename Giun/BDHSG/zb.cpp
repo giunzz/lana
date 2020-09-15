6b9214e77e560cca35879e601ec3a9c9a712d067
diriimq <minhquang116@gmail.com>
2020-09-15 14:07:34 +0700

    sync

---
+++ Giun/BDHSG/zb.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const long long
+#define fname "f"
+
+using namespace std;
+
+cll maxn = 1e3 + 7, maxk = 1e4 + 7;
+ll n, k, f[maxn][maxk] = {{0}};
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen(f".inp", "r", stdin);
+    freopen(f".out", "w", stdout);
+    cin >> n >> k;
+
+}
