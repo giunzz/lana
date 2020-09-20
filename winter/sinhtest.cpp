64b8d4f35cc78c69026b6cf30649459f899b26bc
giunzz <dunghoang0401237@gmail.com>
2020-09-20 19:16:21 +0700

    sync

--- winter/sinhtest.cpp
+++ winter/sinhtest.cpp
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-long long Rand(long long l, long long h)
+#define ll long long
+
+ll giun (ll l , ll h)
 {
-    return l + ((long long)rand() * (RAND_MAX + 1) * (RAND_MAX + 1) * (RAND_MAX + 1) +
-                (long long)rand() * (RAND_MAX + 1) * (RAND_MAX + 1) +
-                (long long)rand() * (RAND_MAX + 1) +
-                rand()) % (h - l + 1);
+    return l+ ((ll) rand() * (RAND_MAX + 1 ) * (RAND_MAX + 1 ) * (RAND_MAX + 1 ) + 
+                (ll) rand() * (RAND_MAX + 1 ) * (RAND_MAX + 1 ) + 
+                (ll) rand() * (RAND_MAX + 1 ) +
+                rand() )  % (h-l+1);
 }
+
 int main()
 {
-    freopen("giun.inp","w",stdout);
-    cout << 40 << endl;
-    int t = 40;
-    while (t--)
-    {
-        int n = Rand(1,100000 );
-        cout << n << " ";
-        int k = Rand(1,5);
-        cout << k << endl;
-        
-}
+    srand(time(NULL));
+        cout << giun(1,10000);
 }
