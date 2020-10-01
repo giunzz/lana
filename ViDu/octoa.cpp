1ee5d90b9f1d572d1620f0252fb5648457a05922
giunzz <dunghoang0401237@gmail.com>
2020-10-01 16:24:08 +0700

    sync

--- ViDu/octoa.cpp
+++ ViDu/octoa.cpp
@@ -7,11 +7,10 @@ string a;
 void tp (string a)
 {
     reverse(a.begin(), a.end()); 
-//    cerr << a << endl;
     for (ll i = 0 ; i < a.size() ; i++)
     {
-        d +=  (a[i]-'0')*pow(8,i)  ;
-//        cerr  <<  a[i]-'0' << " " << pow(8,i) << " " << d << endl; 
+        d +=  (a[i]-'0')*pow(8,i);
+        cerr << d << endl;
     }
 }
 vector<ll> np,he;
@@ -20,28 +19,30 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    freopen("giun.inp","r",stdin);
-    freopen("giun.out","w",stdout);
+    freopen("octoa.inp","r",stdin);
+    freopen("octoa.out","w",stdout);
     cin >> a;
     tp(a);
     d1 = d;
-//    cerr << d1 << endl;
-    
-    while (d){np.push_back(d%2), d/=2;}   
-    reverse(np.begin(), np.end());
-    for (ll i = 0 ; i < np.size() ; i++) cout << np[i];
-    cout << endl;
-
-    while (d1){he.push_back(d1%16), d1/= 16;}   
-    reverse(he.begin(), he.end());
-    for (ll i = 0 ; i < he.size() ; i++) 
+    if (d == 0)
     {
-        if (he[i] == 10) cout << 'A';
-        else  if (he[i] == 11) cout << 'B';
-        else  if (he[i] == 12) cout << 'c';
-        else  if (he[i] == 13) cout << 'D';
-        else  if (he[i] == 14) cout << 'E';
-        else  if (he[i] == 14) cout << 'F';
-        else cout << he[i];
+        cout << 0 << endl << 0;
+        return 0;
     }
+    else 
+    {
+        while (d){np.push_back(d%2), d/=2;}   
+        reverse(np.begin(), np.end());
+        for (ll i = 0 ; i < np.size() ; i++) cout << np[i];
+        cout << endl;
+
+        while (d1){he.push_back(d1%16), d1/= 16;}   
+        reverse(he.begin(), he.end());
+        for (ll i = 0 ; i < he.size() ; i++) 
+        {
+            if(he[i]<10) cout<<he[i];
+            else cout<<char(he[i]%10+65);
+        }
+    }   
+    
 }
