1a0937c33188fe1bbc08d3db80a1dbb99003cf03
DiriiMQ <minhquang116@gmail.com>
2020-10-01 12:30:23 +0700

    sync

--- Giun/BDHSG/test.cpp
+++ Giun/BDHSG/test.cpp
@@ -1,12 +1,44 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
+// using namespace std;
+
+// int main(){
+//     int a, b;
+//     vector<int> cs;
+//     cin >> a >> b;
+//     while(a){
+//         cs.push_back(a%b);
+//         a /= b;
+//     }
+//     reverse(cs.begin(), cs.end());
+//     for(int i : cs) cout << i;
+// }
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+ll a,b,d=0;
+vector<ll> ans;
+int main()
+{
 
-int main(){
-    set<int> a;
-    a.insert(1);
-    a.insert(2);
-    a.insert(3);
-    vector<int> b(a.begin(), a.end());
-    for(int v : b) cerr << v << ' ';
+  cin >> a >> b;
+  while (a != 0)
+  {
+    ans.push_back(a%b);
+    a/=b;
+    //for (ll i = 0 ; i < ans.size() ; i++) cout << ans[i];
+    //cout << endl;
+  }
+  
+  //reverse(ans);
+ // reverse(ans.begin(),ans.end());
+  
+    reverse(ans.begin(), ans.end()); 
+ d = 0;
+  for (ll i = 0 ; i < ans.size() ; i++) 
+  {
+    if ( ans[i]!= 0) d = 1;
+    if (d) cout << ans[i];
+  }
+
 }
