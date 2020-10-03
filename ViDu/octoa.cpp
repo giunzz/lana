5930f234d7e71a94309db6f344ff3816ec0c6dd8
giunzz <dunghoang0401237@gmail.com>
2020-10-03 08:42:50 +0700

    sync

--- ViDu/octoa.cpp
+++ ViDu/octoa.cpp
@@ -1,48 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-ll  d = 0,d1 ;
-string a;
 
-void tp (string a)
-{
-    reverse(a.begin(), a.end()); 
-    for (ll i = 0 ; i < a.size() ; i++)
-    {
-        d +=  (a[i]-'0')*pow(8,i);
-        cerr << d << endl;
+ll change (ll x) {
+    ll p = 0,d = 0;
+    while (x > 0){
+        d += (x % 10) * pow (8, p);
+        p++;
+        x /= 10;
     }
+    return d;
 }
-vector<ll> np,he;
-ll ok = 0;
+
+vector<ll> np;
+vector<char> he;
+ll a,d1,d;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    freopen("giun.inp","r",stdin);
-    freopen("giun.out","w",stdout);
+    freopen("octoa.inp","r",stdin);
+    freopen("octoa.out","w",stdout);
     cin >> a;
-    tp(a);
-    d1 = d;
-    if (d == 0)
+    if (a == 0)
     {
         cout << 0 << endl << 0;
         return 0;
     }
     else 
     {
+        a = change(a),d = a ,d1 = a;
+    
         while (d){np.push_back(d%2), d/=2;}   
         reverse(np.begin(), np.end());
         for (ll i = 0 ; i < np.size() ; i++) cout << np[i];
         cout << endl;
 
-        while (d1){he.push_back(d1%16), d1/= 16;}   
-        reverse(he.begin(), he.end());
-        for (ll i = 0 ; i < he.size() ; i++) 
+        while (d1)
         {
-            if(he[i]<10) cout<<he[i];
-            else cout<<char(he[i]%10+65);
-        }
+            char x = d1 % 16 + '0';
+		    if (x > 57) x = char (x - 58 + 'A');
+		    he.push_back (x);
+		    d1 /= 16;
+        }   
+        reverse(he.begin(), he.end());
+        for (ll i = 0 ; i < he.size() ; i++) cout << he[i];
     }   
     
 }
