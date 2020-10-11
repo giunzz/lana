559b4f8cac90ec098046f9e306367d60b22146c4
giunzz <dunghoang0401237@gmail.com>
2020-10-12 00:10:09 +0700

    sync

---
+++ ViDu/CONSEQ.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define giuncute ios_base::sync_with_stdio(0), cin.tie(0);
+#pragma GCC optimize("Ofast")
+ll n , a[(ll) 1e6+7], f[(ll) 1e6+7]={0} ;
+int main()
+{
+    giuncute;
+    cin >> n ;
+    for (int i = 1 ; i <= n ; i++) cin >> a[i];
+    f[0] = 0;
+    for (int i = 1 ; i <= n ; i++)
+    {
+        for (int j = 1 ;  j <= i ; j++)
+        {
+            if (a[if[i] = max (f[i-1])
+        }
+    }
+} 
