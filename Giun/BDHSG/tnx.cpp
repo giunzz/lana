546b8c35590e6dbfe9af1684b2f4104ae301ca2e
DiriiMQ <minhquang116@gmail.com>
2020-10-16 21:04:20 +0700

    sync

--- Giun/BDHSG/tnx.cpp
+++ Giun/BDHSG/tnx.cpp
@@ -2,7 +2,7 @@
 #define ll long long
 #define cll const ll
 #define lp(a, b, c) for(ll a = b; a <= c; ++a)
-#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
 #define vec(a) vector<a>
 #define pp(a, b) pair<a, b>
 #define Fname "f"
@@ -13,25 +13,19 @@ void OF(){
     freopen(Fname ".out", "w", stdout);
 }
 
-cll maxm = 1e5 + 7;
-ll n, cnt[maxm] = {0};
+ll n, k;
 vec(ll) a;
-
-vec(ll) cpress(vec(ll) x){
-    set<ll> tmp(x.begin()+1, x.end());
-    vec(ll) tt(tmp.begin(), tmp.end());
-    return tt;
-}
+vec(vec(ll)) f;
 
 int main(){
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0); 
+    cin.tie(0); cout.tie(0);
     OF();
     cin >> n;
-    a.resize(n + 2);
-    lp(i, 1, n + 1){
-        cin >> a[i];
-        ++cnt[a[i]];
+    a.resize(n + 1);
+    lp(i, 1, n) cin >> a[i];
+    lp(i, 0, n) f.push_back(vec(ll)(k, 0)); 
+    lp(i, 1, n){
+        
     }
-    a = cpress(a);
 }
