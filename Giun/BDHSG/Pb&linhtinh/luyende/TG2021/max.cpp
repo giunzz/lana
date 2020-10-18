32fa5f74dc7d61677bba865457e59fda38fd90c6
giunzz <dunghoang0401237@gmail.com>
2020-10-18 11:20:40 +0700

    sync

---
+++ Giun/BDHSG/Pb&linhtinh/luyende/TG2021/max.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define fname "f"
+using namespace std;
+
+void OF(){
+    freopen(fname ".inp", "r", stdin);
+    freopen(fname ".out", "w", stdout);
+}
+
+ll n, m, c;
+string s;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> c >> n >> m >> s;
+    
+}
