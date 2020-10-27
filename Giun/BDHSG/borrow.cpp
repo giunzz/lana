f5cfc76a5c15f9a30ba498db04833658a5374a09
DiriiMQ <minhquang116@gmail.com>
2020-10-27 10:19:48 +0700

    sync

--- Giun/BDHSG/borrow.cpp
+++ Giun/BDHSG/borrow.cpp
@@ -13,8 +13,54 @@ void OF(){
     freopen(Fname ".out", "w", stdout);
 }
 
+cll maxn = 2e5 + 7;
+ll n, dC[maxn] = {0}, k = 0;
+bool d[maxn] = {0}; 
+vec(pp(ll, ll)) g[maxn], preg[maxn];
+
+void init(){
+    ll v, c;
+    lp(u, 1, n){
+        cin >> v >> c;
+        cerr << u  << ' ' << v << ' ' << c << '\n';
+        g[u].push_back({c, v});
+        preg[v].push_back({c, u});
+    }
+}
+
+#define pos second
+#define cost first
+
+void getCycle(ll &r){
+    
+}
+
+void findCycle(ll &r){
+    ll u = r;
+    // pp(ll, ll) v = g[u];
+    // if(d[v.pos]){
+    //     if(dC[v.pos]) return;
+    //     ++k;
+    //     selectCycle(v.pos);
+    // }
+    while(g[u].size()){
+        pp(ll, ll) v = g[u].back();
+        if(d[v.pos]){
+            getCycle(v.pos);
+            break;
+        }
+        d[v.pos] = 1;
+        u = v.pos;
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     OF();
+    cin >> n;
+    init();
+    lp(i, 1, n){
+        if(!d[i]) findCycle(i);
+    }
 }
