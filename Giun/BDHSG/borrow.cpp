cec6b7404a98d6e2bf75ffbf7cbf4fc0188367ef
DiriiMQ <minhquang116@gmail.com>
2020-10-27 13:18:20 +0700

    sync

--- Giun/BDHSG/borrow.cpp
+++ Giun/BDHSG/borrow.cpp
@@ -14,7 +14,7 @@ void OF(){
 }
 
 cll maxn = 2e5 + 7;
-ll n, k = 0, ans = 0, tt = 0;
+ll n, k = 0, ans = 0, tt = 0, db[maxn] = {0};
 bool dC[maxn] = {0}, d[maxn] = {0}; 
 vec(pp(ll, ll)) g[maxn], preg[maxn];
 
@@ -31,27 +31,56 @@ void init(){
 #define pos second
 #define cost first
 
-ll res;
-vec(ll) vl;
+#define fi first
+#define se second
 
-ll dfs(ll &u){
-    ll tmp = 0;
-    for(pp(ll, ll) &v : g[u]){
+ll res;
+vec(ll) vl, tg;
+
+pp(ll, ll) dfs(ll &u, ll const &r){
+    ll tmp = 0, tmp1 = 0;
+    for(pp(ll, ll) &v : preg[u]){
+        if(dC[v.pos]) continue;
         d[v.pos] = 1;
-        tmp = max
+        tmp += dfs(v.pos, r).fi; //so thanh pho do vao
+        tmp1 += v.cost; //so tien hien co
     }
+    if(tmp1 < g[u].back().cost && u != r)  tmp += g[u].back().cost - tmp1;
+    return {tmp, tmp1};
 }
 
 void getCycle(ll &r){
+    bool ok = 0;
+    ll mindb = LLONG_MAX;
+    // cerr << r << '\n';
     vl.clear();
     ll u = r;
     while(!dC[u]){
-        pp(ll, ll) v = g[u];
-        vl.push_back(v.cost);
+        pp(ll, ll) v = g[u].back();
+        vl.push_back(u);
+        mindb = min(mindb, v.cost);
         dC[u] = 1;
-        if(preg[u].size() > 1) dfs(u);
         u = v.pos;
     }
+    // cerr << ans << '\n';
+    lp(i, 0, vl.size() - 1){
+        // cerr << u << ' ';
+        u = vl[i];
+        if(preg[u].size() > 1){
+            pp(ll, ll) tmp = dfs(u, u);
+            db[u] = tmp.se;
+            ans += tmp.fi;
+        }
+        ll tmp1 = db[u] + g[vl[(i - 1 + vl.size()) % vl.size()]].back().cost;
+        // cerr << u << ' ' << db[u] << '\n';
+        if(tmp1 < g[u].back().cost){
+            ans += g[u].back().cost - tmp1;
+            ok = 1;
+        }
+    }
+    // cerr << mindb;
+    if(!ok) ans += mindb;
+    // cerr << '\n' << ans;
 }
 
 void findCycle(ll &r){
@@ -83,4 +112,5 @@ int main(){
     lp(i, 1, n){
         if(!d[i]) findCycle(i);
     }
+    cout << ans;
 }
