db4b9431911a84ea7c21246a5ad330dea915f18f
DiriiMQ <minhquang116@gmail.com>
2020-10-28 19:49:12 +0700

    sync

--- BHuong/SIEUNTO.cpp
+++ BHuong/SIEUNTO.cpp
@@ -4,15 +4,15 @@ using namespace std;
 #define giuncute ios_base::sync_with_stdio(0),cin.tie(0);
 #define ballolo freopen("SIEUNTO.inp","r",stdin) , freopen("SIEUNTO.out","w",stdout) ; 
 #pragma GCC optimize("Ofast")
-const ll nmax = 1e8 + 7;
+const ll nmax = 1e7 + 7;
 ll sang[nmax], n , x;
 
 void xaysang()
 {
     sang[0] = 1; sang[1] = 1;
-    for (int i = 2; i * i <= 1e8; i++)
+    for (int i = 2; i * i <= 1e7; i++)
         if (!sang[i]) 
-           for (int j = i * 2; j <= 1e8; j += i) sang[j] = 1;
+           for (int j = i * 2; j <= 1e7; j += i) sang[j] = 1;
 }
 
 bool check (ll k)
@@ -32,6 +32,7 @@ int main()
     ballolo;
     cin >> n ;
     xaysang();
+    // cerr << clock();
     for (int i = 1 ; i <= n ; i++)
     {
         cin >> x;
