628b6f08d86d4d55b82d6ca87f72089f3afce24d
dirii <minhquang116@gmail.com>
2020-11-02 13:16:15 +0700

    sync

---
+++ Giun/hackerrank/morgan-and-a-string.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "f"
+using namespace std;
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+string process(string a, string b){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    OF();
+    ll t;
+    string a, b;
+    cin >> t;
+    while(t--){
+        cin >> a >> b;
+
+    }
+}
