962f4c37f5e7558edc9b27ceac5ed4c513d81824
dirii <minhquang116@gmail.com>
2020-11-02 10:26:07 +0700

    sync

---
+++ Giun/BDHSG/bieudo.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "f"
+using namespace std;
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+cll maxn = 1e5 + 7;
+ll n, a[maxn];
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    OF();
+    cin >> n;
+
+}
