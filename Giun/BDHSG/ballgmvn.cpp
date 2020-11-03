01456e10ae3cc4628e239ea777f9f66192241260
dirii <minhquang116@gmail.com>
2020-11-03 17:52:30 +0700

    sync

--- Giun/BDHSG/ballgmvn.cpp
+++ Giun/BDHSG/ballgmvn.cpp
@@ -26,17 +26,69 @@ public:
     }
 };
 
-cll maxn = 1e3 + 8;
-ll n;
-pp(ll, ll) red[maxn], grn[maxn], f[maxn][maxn];
-unordered_map<pp(ll, ll), pp(ll, ll), pairhash> mp;
+// cll maxn = 1e3 + 8;
+// ll n;
+// pp(ll, ll) red[maxn], grn[maxn], f[maxn][maxn];
+// unordered_map<pp(ll, ll), pp(ll, ll), pairhash> mp;
 
-#define x first
+// #define x first
+// #define y second
+
+// int main(){
+//     opt;
+//    // file;
+    // cin >> n;
+    // lp(i, 1, n)cin >> grn[i].x >> grn[i].y;
+    // lp(i, 1, n)cin >> red[i].x >> red[i].y;
+//     lp(i, 1, n){
+//         lp(j, 1, n){
+            // pp(ll, ll) vt = {red[j].x - grn[i].x, red[j].y - grn[i].y};
+            // ll tmp  = __gcd(vt.x, vt.y);
+            // vt.x /= tmp, vt.y /= tmp;
+            // if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
+//             f[i][j] = vt; 
+//             mp[vt] = {i, j};
+//         }
+//     }
+//     ll pos;
+//     lp(i, 1, n){
+//         lp(j, i + 1, n){
+//             pp(ll, ll) vt = {grn[j].x - grn[i].x, grn[j].y - grn[i].y};
+//             ll tmp  = __gcd(vt.x, vt.y);
+//             vt.x /= tmp, vt.y /= tmp;
+//             if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
+//             if((pos = mp[vt].y) && (f[i][pos] == vt)){
+//                 cout << i << ' ' << j << ' ' << (pos + n) << '\n';
+//                 return 0;
+//             }
+//         }
+//     }
+//     lp(i, 1, n){
+//         lp(j, i + 1, n){
+//             pp(ll, ll) vt = {red[j].x - red[i].x, red[j].y - red[i].y};
+//             ll tmp  = __gcd(vt.x, vt.y);
+//             vt.x /= tmp, vt.y /= tmp;
+//             if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
+//             if((pos = mp[vt].x) && (f[pos][i] == vt)){
+//                 cout << (i + n) << ' ' << (j + n) << ' ' << mp[vt].x;
+//                 return 0;
+//             }
+//         }
+//     }
+//     cout << -1;
+// }
+
+cll maxn = 2e3 + 8;
+ll n, pos;
+pp(ll, ll) red[maxn], grn[maxn];
+unordered_map<pp(ll, ll), ll, pairhash> mp[maxn];
+
+#define x first 
 #define y second
 
 int main(){
-    opt;
-   // file;
+    opt; 
+    file;
     cin >> n;
     lp(i, 1, n)cin >> grn[i].x >> grn[i].y;
     lp(i, 1, n)cin >> red[i].x >> red[i].y;
@@ -46,33 +98,14 @@ int main(){
             ll tmp  = __gcd(vt.x, vt.y);
             vt.x /= tmp, vt.y /= tmp;
             if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
-            f[i][j] = vt; 
-            mp[vt] = {i, j};
-        }
-    }
-    ll pos;
-    lp(i, 1, n){
-        lp(j, i + 1, n){
-            pp(ll, ll) vt = {grn[j].x - grn[i].x, grn[j].y - grn[i].y};
-            ll tmp  = __gcd(vt.x, vt.y);
-            vt.x /= tmp, vt.y /= tmp;
-            if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
-            if((pos = mp[vt].y) && (f[i][pos] == vt)){
-                cout << i << ' ' << j << ' ' << (pos + n) << '\n';
+            if(pos = mp[i][vt]){
+                cout << i << ' ' << (j + n) << ' ' << pos;
+                return 0;
+            } else if(pos = mp[j + n][vt]){
+                cout << i << ' ' << (j + n) << ' ' << pos;
                 return 0;
             }
-        }
-    }
-    lp(i, 1, n){
-        lp(j, i + 1, n){
-            pp(ll, ll) vt = {red[j].x - red[i].x, red[j].y - red[i].y};
-            ll tmp  = __gcd(vt.x, vt.y);
-            vt.x /= tmp, vt.y /= tmp;
-            if(vt.x < 0) vt.x = -vt.x, vt.y = -vt.y;
-            if((pos = mp[vt].x) && (f[pos][i] == vt)){
-                cout << (i + n) << ' ' << (j + n) << ' ' << mp[vt].x;
-                return 0;
-            }
+            mp[i][vt] = j + n, mp[j + n][vt] = i;
         }
     }
     cout << -1;
