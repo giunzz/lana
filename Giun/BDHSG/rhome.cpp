7160d627c0f1a67e7e80efccc266980a5cd9dd4a
dirii <minhquang116@gmail.com>
2020-11-04 12:04:23 +0700

    sync

--- Giun/BDHSG/rhome.cpp
+++ Giun/BDHSG/rhome.cpp
@@ -5,7 +5,8 @@
 #define lpd(a, b, c) for(ll a = b; a >= c; --a)
 #define vec(a) vector<a>
 #define pp(a, b) pair<a, b>
-#define Fname "rhome"
+// #define Fname "rhome"
+#define Fname "f"
 using namespace std;
 
 void OF(){
@@ -52,7 +53,9 @@ void dijkstra(ll const &r){
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    #ifdef DEBUG
     OF();
+    #endif
     cin >> n >> m;
     cin >> a[0].x >> a[0].y >> a[m + 1].x >> a[m + 1].y;
     a[0].z = 0, a[m + 1].z = m + 1;
@@ -60,8 +63,12 @@ int main(){
         cin >> a[i].x >> a[i].y;
         a[i].z = i;
     }
-    sort(a, a + 2 + m, cpr1);
-    lp(i, 1, m + 1){
+    lp(i, 1, m){
+        g[0].push_back({min((a[i].x - a[0].x), abs(a[i].y - a[0].y)), a[i].z});
+        g[a[i].z].push_back({abs(a[i].x - a[m + 1].x) + abs(a[i].y - a[m + 1].y), m + 1});
+    }
+    sort(a + 1, a + 1 + m, cpr1);
+    lp(i, 2, m){ 
         g[a[i].z].push_back({a[i].x - a[i - 1].x, a[i - 1].z});
         g[a[i - 1].z].push_back({a[i].x - a[i - 1].x, a[i].z});
     }
@@ -69,8 +76,8 @@ int main(){
     // g[0].push_back({abs(a[1].x - a[0].x) + abs(a[1].y - a[0].y), a[1].z});
     // g[a[m].z].push_back({abs(a[m].x - a[m + 1].x) + abs(a[m].y - a[m + 1].y), m + 1});
     // g[m + 1].push_back({abs(a[m].x - a[m + 1].x) + abs(a[m].y - a[m + 1].y), a[m].z});
-    sort(a, a + 2 + m, cpr2);
-    lp(i, 1, m + 1){
+    sort(a, a + 1 + m, cpr2);
+    lp(i, 2, m){
         g[a[i].z].push_back({a[i].y - a[i - 1].y, a[i - 1].z});
         g[a[i - 1].z].push_back({a[i].y - a[i - 1].y, a[i].z});
     }
--- Giun/hackerrank/morgan-and-a-string.cpp
+++ Giun/hackerrank/morgan-and-a-string.cpp
@@ -22,6 +22,6 @@ int main(){
     cin >> t;
     while(t--){
         cin >> a >> b;
-
+        
     }
 }
