409bd73eb85a6389cef9f62ac6bc5fe5bb96ab8a
dirii <minhquang116@gmail.com>
2020-11-10 00:02:29 +0700

    sync

--- Python/testbackend/route.py
+++ Python/testbackend/route.py
@@ -1,36 +1,33 @@
-import sys, time
-from flask import Flask, redirect, url_for 
+import os
+from flask import Flask, redirect, url_for, send_file
 
 # directfilelog = 'python/testbackend/f.log'
 app = Flask(__name__)
+root = 'C:/Users/tienp/Desktop/Quang/Hoc/Giun/BDHSG/Pb&linhtinh/Tung(BKHN)/'
 
 @app.route('/')
-def helloword():
-    # with open(directfilelog, 'a') as log:
-    #     current_time = time.strftime("%H:%M:%S", time.localtime())
-    #     log.write(current_time + ' test log\n')
-    return 'Hello!'
-    
-@app.route('/abc/dc')
-def abc():
-    # with open(directfilelog, 'a') as log:
-    #     current_time = time.strftime("%H:%M:%S", time.localtime())
-    #     log.write(current_time + ' test log\n')
-    return 'abc'
+def show():
+    ext = ''
+    getFolder = os.listdir(root)
+    for name in getFolder:
+        merge = name.split('.')
+        if(merge[len(merge) - 1] == 'pdf'):
+            ext += name + '<br/>'
+    # print(ext)
+    return ext
 
+@app.route('/t/<name_file>')
+def download(name_file):
+    path = root + name_file
+    return send_file(path, as_attachment=True)
 
-@app.route('/user/<name>')
-def adminpage(name):
-    # with open(directfilelog, 'a') as log:
-    #     log.write('test log 1\n')
-    return 'hello %s' % name
+# @app.route('/user/<name>')
+# def adminpage(name):
+#     return 'hello %s' % name
 
-
-@app.route('/quang')
-def quang():
-    return redirect(url_for('abc/dc'))
+# @app.route('/quang')
+# def quang():
+#     return redirect(url_for('adminpage', name='quang'))
 
 if __name__ == '__main__':
-    # with open(directfilelog, 'w') as log:
-    #     log.write('test log 2\n')
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
