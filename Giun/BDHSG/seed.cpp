c542347203fffb9c4ddbb50532f99f4e1f2c96d1
dirii <minhquang116@gmail.com>
2020-11-16 07:31:47 +0700

    sync

--- Giun/BDHSG/seed.cpp
+++ Giun/BDHSG/seed.cpp
@@ -13,9 +13,99 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
+cll maxn = 1e6;
+ll n, bs = 0, trie[maxn][2], j[maxn][2], Nnode = 1;
+bool lf[maxn] = {0};
+string s;
+
+void push(ll num){
+    ll cur = 0;
+    lpd(i, s.size() - 1, 0){
+        if(trie[cur][(num >> i) & 1] == -1){
+            trie[cur][(num >> i) & 1] = Nnode;
+            Nnode++;
+        }
+        cur = trie[cur][(num >> i) & 1];
+    }
+    lf[cur] = 1;
+}
+
+void gene(ll pos, ll tt){
+    // cerr << pos << ' ' << tt << '\n';
+    if(pos == s.size()){
+        // cerr << tt << ' ';
+        push(tt);
+        // cerr << 1;
+        return;
+    }
+    ll ntt;
+    if(s[pos] == '*'){
+        lp(i, 0, 1){
+            ntt = tt | ((i * 1LL) << pos);
+            // cerr << (tt | (1 << pos)) << '\n';
+            gene(pos + 1, ntt);
+            // cerr << tt << ' ';
+        }
+    }
+    else gene(pos + 1, tt);
+}
+
+ll find_j(ll stt, ll v, ll cnt){
+    ll sttp = (stt << 1LL) | v, ncnt = cnt + 1;
+    while(cnt){
+        sttp = sttp & ~(1LL << (ncnt-- - 1));
+        // bool ok = 0;
+        ll c = 0;
+        lpd(j, cnt - 1, 0){
+            if(trie[c][sttp & (1LL << j)] > -1){
+                c = trie[c][sttp & (1LL << j)];
+            }
+            else{
+                break;
+            }
+        }
+        return c;
+    }
+    return 0;
+}
+
+void dfs(ll i, ll stt, ll cnt){
+    lp(v, 0, 1){
+        if(trie[i][v] > -1){
+            j[i][v] = trie[i][v];
+            dfs(trie[i][v], (stt << 1) | v, cnt + 1);
+        }
+        else
+            j[i][v] = find_j(stt, v, cnt);
+    }
+}
+
+ll sol(ll pos, ll node){
+    if(pos > n) return 1;
+    ll cnt = 0;
+    lp(v, 0, 1){
+        if(!lf[j[node][v]]){
+            cnt += sol(pos + 1, j[node][v]);
+        }
+    }
+    return cnt;
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    OF();
-    
+    // OF();
+    cin >> n >> s;
+    lp(i, 0, s.size() - 1){
+        if(s[i] == '1') bs = bs | (1LL << i);
+    }
+    // cerr << bs;
+    memset(trie, -1, sizeof(trie));
+    memset(j, -1, sizeof(j));
+    gene(0, bs);
+    // cerr << lf[5];
+    // cerr << Nnode;
+    dfs(0, 0, 0);
+    // cerr << j[4][0];
+    cout << (1LL << n) - sol(1, 0);
 }
--- Giun/BDHSG/tempCodeRunnerFile.cpp
+++ Giun/BDHSG/tempCodeRunnerFile.cpp
@@ -1,5 +1,1 @@
-
-                while(pq.size()){
-                    pp(quang, ll) tmp1 = pq.top();
-                    pq.pop();
-                    if(chose[tmp1.se] || tmp1
+tt | (1 << pos)
