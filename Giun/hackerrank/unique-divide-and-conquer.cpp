77207ada39447879456ae4025c315821fad36639
dirii <minhquang116@gmail.com>
2020-11-24 14:07:55 +0700

    sync

---
+++ Giun/hackerrank/unique-divide-and-conquer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "f"
+using namespace std;
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+cll maxn = 3e3 + 7;
+ll n, MOD, f[maxn], forest[maxn];
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    OF();
+    cin >> n >> MOD;
+    lp(i, 1, n){
+        
+    }
+}
