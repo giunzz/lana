e84f51d89dedbd1f66d67358e1a7be593e37d838
dirii <minhquang116@gmail.com>
2020-11-30 17:22:37 +0700

    sync

--- Giun/BDHSG/custom/rel/rel.cpp
+++ Giun/BDHSG/custom/rel/rel.cpp
@@ -13,40 +13,37 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
-using point = pp(ll, ll);
-cll maxn = 1e5 + 7;
-ll n, m, l, r; 
-point sh[maxn], cp[maxn];
-
 #define x first
 #define y second
 #define rt 1
 #define lt -1
 #define ga 0
 
+using point = pp(ll, ll); 
+cll maxn = 1e5 + 7;
+ll n, m, l, r, res[maxn] = {0}; 
+point sh[maxn], cp[maxn];
+
 inline ll area(point &a, point &b, point &c){
     point vecab = {b.x - a.x, b.y - a.y}, vecbc = {c.x - b.x, c.y - b.y};
     return vecab.x * vecbc.y - vecbc.x *  vecab.y;
 }
 
-ll ccw(point a, point b, point c){
+inline ll ccw(point a, point b, point c){
     ll s = area(a, b, c);
     if(s > 0) return 1;
     if(!s) return 0;
     return -1;
 }
 
-vec(point) r;
-
 void prc(){
-    ll rq;
-    lp(i, 0, n - 1){
-        if((rq = ccw(sh[1], cp[i], cp[(i + 1) % n])) == ccw(sh[i], cp[i], cp[(i - 1 + n) % n])){
-            if(rq == lt) l = i;
-            else r = i;
-        }
+    ll direction;
+    lp(i, 1, m){
+        ll pre = ccw(sh[1], cp[i], cp[i - 1]), next = ccw(sh[1], cp[i], cp[i + 1]);
+        if(pre * -1 == next) continue;
+        if(next == 1) r = i;
+        else if(pre == -1) l = i;
     }
-    
 }
 
 int main(){
@@ -54,7 +51,7 @@ int main(){
     cin.tie(0); cout.tie(0);
     OF();
     cin >> n;
-    lp(i, 0, n - 1){
+    lp(i, 1, n){
         ll x, y;
         cin >> x >> y;
         sh[i] = {x, y};
@@ -65,5 +62,15 @@ int main(){
         cin >> x >> y;
         cp[i] = {x, y};
     }
+    cp[0] = cp[m], cp[m + 1] = cp[1];
+    prc(); // find 2 tangent of sh1 to convex polygon
+    cerr << l << ' ' << r;
 
 }
+
+/*
+https://geogebra.org/classic/rjhvxukk
+https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+https://nodejs.dev/learn
+https://onedrive.live.com/?authkey=%21ANfPeYx6k3o2AKA&id=A6081E19970D24F2%2124698&cid=A6081E19970D24F2
+*/
