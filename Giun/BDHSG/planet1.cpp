65e9bed86528d7ce724d69f2d2ac213fc4dc8852
dirii <minhquang116@gmail.com>
2020-12-03 08:44:23 +0700

    sync

--- Giun/BDHSG/planet1.cpp
+++ Giun/BDHSG/planet1.cpp
@@ -16,17 +16,20 @@ void OF(){
 cll maxn = 507;
 ll n, m, dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1}, a[maxn][maxn] = {{0}}, d[3][4] = {{0, 0, 0, 0}, {3, 2, 1, 0}, {1, 0, 3, 2}},
 dp[maxn][maxn][4] = {{{0}}};
+bool dd[maxn][maxn] = {{0}};
 
-ll dfs(ll x, ll y, ll h){
+inline ll dfs(ll x, ll y, ll h, ll const &rx, ll const &ry){
+    if(x == rx && y == ry) return INT_MAX;
     if(x == 0 || x == n + 1 || y == 0 || y == m + 1 || !a[x][y]) return 0;
     if(dp[x][y][h]) return dp[x][y][h];
     if(a[x][y] == -1){
-        dp[x][y][h] = dfs(x + dx[h], y + dy[h], h) + 1;
+        dp[x][y][h] = dfs(x + dx[h], y + dy[h], h, rx, ry) + 1;
     }
     else{
         ll nh = d[a[x][y]][h];
-        dp[x][y][h] = dfs(x + dx[nh])
+        dp[x][y][h] = dfs(x + dx[nh], y + dy[nh], nh, rx, ry) + 1;
     }
+    return dp[x][y][h];
 }
 
 int main(){
@@ -46,5 +49,8 @@ int main(){
     ll x, y;
     cin >> x >> y;
     ll ans = LLONG_MIN;
-    lp(i, 0, 3) ans = max(ans, dfs(x + dx[i], y + dy[i], i) + 1);
+    dd[x][y] = 1;
+    lp(i, 0, 3) ans = max(ans, dfs(x + dx[i], y + dy[i], i, x, y) + 1);
+    if(ans < INT_MAX) cout << ans;
+    else cout << "Voyager";
 }
