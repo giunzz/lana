899be1cfb751e2ab60c40f5cba3a06a494481875
dirii <minhquang116@gmail.com>
2020-12-10 13:05:00 +0700

    sync

---
+++ Giun/BDHSG/Pb&linhtinh/luyende/SpringContest21/Day2/spc5/spc5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const long long
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+using namespace std;
+
+void file(const string file){
+    freopen((file + ".inp").c_str(), "r", stdin);
+    freopen((file + ".out").c_str(), "w", stdout);
+}
+
+int main(){
+    file("pb");
+}
