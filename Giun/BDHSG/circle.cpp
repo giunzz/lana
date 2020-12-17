581b8e0b747cbaf160db995e4b4a9b725a643ba7
dirii <minhquang116@gmail.com>
2020-12-17 14:03:28 +0700

    sync

---
+++ Giun/BDHSG/circle.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "f"
+using namespace std;
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+cll maxn = 1e5 + 7;
+ll n;
+unordered_map<ll, ll> update;
+pp(ll, ll) a[maxn];
+
+bool cpr(pp(ll, ll) &x, pp(ll, ll) &y){
+    return x.second < y.second;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    OF();
+    cin >> n;
+    lp(i, 1, n)
+        cin >> a[i].first >> a[i].second;
+    ll ans = 1;
+    sort(a + 1, a + 1 + n, cpr);
+    lp(i, 1, n){
+        ll r = a[i].second, x = a[i].first;
+        ll cnt = 1, tmp = x + r, t;
+        while(update.find(tmp) != update.end() && (t = update[tmp]) >= x - r)
+            tmp = t;
+        if(tmp == x - r) ++cnt;
+        ans += cnt;
+        update[x + r] = x - r;
+    }
+    cout << ans;
+}
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int a[10], d[10]={0};
+
+
+// void check()
+// {
+//     int sum = 0 , sl = 0;
+//     for (int i = 1 ; i <= 9 ; i++)
+//     {
+//         if (d[i] == 1) sum += a[i] , sl++;
+//     }
+//     if (sum == 10 && sl == 7)
+//     {
+//         for (int i = 1 ; i <= 9 ; i++)
+//         {
+//             if (d[i] == 1) cout << a[i] << " ";
+//             break;
+//         }   
+//     }
+// }
+// void quaylui (int vt)
+// {
+//     if (vt == 9) check();
+//     else 
+//         for (int i =0 ; i <= 1 ; i++)
+//         {
+//             d[vt] = 1;
+//             quaylui(vt+1);
+//         }
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(0) ;
+//     cin.tie(0);
+//     for (int i = 1 ; i <= 9 ; i++) cin >> a[i];
+//     quaylui(1);
+
+// }
