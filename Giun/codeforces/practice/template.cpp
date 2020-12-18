8f0386938fc12919c931b8148e7c7c9809554f96
DiriiMQ <minhquang116@gmail.com>
2020-12-18 13:49:15 +0700

    sync

---
+++ Giun/codeforces/practice/template.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "f"
+using namespace std;
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    OF();
+    #endif
+    
+}
