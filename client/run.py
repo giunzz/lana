31deadea0aed3e145ba4f037eae62541e13582cc
DiriiMQ <minhquang116@gmail.com>
2020-12-31 15:23:03 +0700

    sync

--- client/run.py
+++ client/run.py
@@ -1,10 +1,17 @@
 import os
+from time import sleep
+from datetime import datetime
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
 global locationGit
 
 def joinPath(name1, name2):
     return os.path.join(name1, name2)
+
+def wRunClient(info):
+    now = datetime.now().strftime('%d/%m/%Y %H:%M:%S ---> ')
+    with open(joinPath(joinPath(__location__, 'log'), 'status.log'), 'a') as wLog:
+        wLog.write(now + info + '\n')
 
 def getLocationGit():
     global locationGit
@@ -13,6 +20,27 @@ def getLocationGit():
     listLoca[0] = listLoca[0] + '\\'
     locationGit = os.path.join(*listLoca)
 
+def gitPull():
+    os.system('cd %s && git pull origin quang' % locationGit)
+
+def gitPush():
+    os.system('cd %s && gts quang' % locationGit)
+
+def statusConnect():
+    open(joinPath(__location__, 'ping.txt'), 'w')
+    while True:
+        gitPush()
+        readPing = open(joinPath(__location__, 'ping.txt'), 'r')
+        content = readPing.read()
+        content.split()
+        if len(content):
+            break
+        sleep(10)
+
+def statusProb():
+
+
 if __name__ == "__main__":
-    getLocationGit()
-    print(locationGit)
+    # getLocationGit()
+    # print(locationGit)
+    statusConnect()
--- client/test.py
+++ client/test.py
@@ -2,12 +2,17 @@ import os, subprocess
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
 
+location = 'C:\\Users\\tienp\\Desktop\\Hoc'
+
 # subprocess.Popen([os.path.join(__location__, '00_10Tin_BTTH_Tonghop.pdf')], shell=True)
-subprocess.Popen(['fc', os.path.join(__location__, 'test.txt', os.path.join(__location__, 'test1.txt'), '/w')], shell=True)
+# subprocess.Popen(['fc', os.path.join(__location__, 'test.txt', os.path.join(__location__, 'test1.txt'), '/w')], shell=True)
 
 import time
 
-time.sleep(10)
+# time.sleep(10)
+tmp = os.system('cd %s && git add . && git commit -m "sync" && git push -u origin quang' % location)
 
-with open(os.path.join(__location__, 'test.txt'), 'w') as wlog:
-    wlog.write('testttttttt')
+print(tmp)
+
+# with open(os.path.join(__location__, 'test.txt'), 'w') as wlog:
+#     wlog.write('testttttttt')
