33d30ca0127b4ccf4cd6ca369e34b5ad771de6a1
DiriiMQ <minhquang116@gmail.com>
2020-12-31 16:40:57 +0700

    "sync"

--- client/run.py
+++ client/run.py
@@ -1,10 +1,12 @@
-import os, filecmp
+import os, filecmp, subprocess
 from distutils.dir_util import copy_tree
+from shutil import copyfile
 from time import sleep
 from datetime import datetime
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
 global locationGit
+listProbs = []
 
 def joinPath(name1, name2):
     return os.path.join(name1, name2)
@@ -22,33 +24,50 @@ def getLocationGit():
     locationGit = os.path.join(*listLoca)
 
 def gitPull():
-    os.system('cd %s && git pull origin quang' % locationGit)
+    with open(joinPath(__location__, 'log\\temp.log'), 'w') as wlog:
+        subprocess.run(["cd", locationGit, "&&", "git", "pull", "origin", "quang"], stdout=wlog, stderr=wlog, shell=True)
 
 def gitPush():
-    os.system('cd %s && gts quang' % locationGit)
+    gitPull()
+    with open(joinPath(__location__, 'log\\temp.log'), 'w') as wlog:
+        subprocess.run(["cd", locationGit, "&&", "git", "add", ".", "&&", "git", "commit", "-m", "\"sync\"", "&&", "git", "push", "-u", "origin", "quang"], stdout=wlog, stderr=wlog, shell=True)
 
 def statusConnect():
     open(joinPath(__location__, 'ping.txt'), 'w')
     while True:
         gitPush()
         readPing = open(joinPath(__location__, 'ping.txt'), 'r')
-        content = readPing.read()
-        content.split()
+        content = readPing.read().split()
         if len(content):
             break
         sleep(10)
+
+def statusProb():
+    global listProbs
+    while not os.path.exists(joinPath(__location__, 'probs')) or len(os.listdir(joinPath(__location__, 'probs'))) == 0:
+        gitPull()
+        sleep(10)
+    readPing = open(joinPath(__location__, 'ping.txt'), 'r')
+    listProbs = readPing.read().split()
 
 def statusTasks():
     cprTasksTemp = filecmp.dircmp(joinPath(__location__, 'tasks'), joinPath(__location__, 'temp'))
     dirTasks = joinPath(__location__, 'tasks')
     dirTemp = joinPath(__location__, 'temp')
     checkStatus = 0
-    if len(cprTasksTemp.left_only):
-        copy_tree(dirTasks, dirTemp)
-        checkStatus = 1
+    for nameTask in cprTasksTemp.left_only:
+        splitTask = nameTask.split('.')
+        if len(splitTask) == 2 and splitTask[1] == '.cpp' and splitTask[0] in listProbs:
+            checkStatus = 1
+            copyfile(joinPath(dirTasks, nameTask), joinPath(dirTemp, nameTask))
     return checkStatus
 
 if __name__ == "__main__":
-    # getLocationGit()
+    print(0)
+    getLocationGit()
     # print(locationGit)
+    print(1)
     statusConnect()
+    print(2)
+    statusProb()
+    print(listProbs)
--- client/test.py
+++ client/test.py
@@ -2,7 +2,7 @@ import os, subprocess, filecmp
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
 
-location = 'C:\\Users\\tienp\\Desktop\\Hoc'
+locationGit = 'C:\\Users\\tienp\\Desktop\\Hoc'
 
 # subprocess.Popen([os.path.join(__location__, '00_10Tin_BTTH_Tonghop.pdf')], shell=True)
 # subprocess.Popen(['fc', os.path.join(__location__, 'test.txt', os.path.join(__location__, 'test1.txt'), '/w')], shell=True)
@@ -15,8 +15,10 @@ import time
 # os.system('git add . && git commit -m "sync" && git push -u origin quang')
 # subprocess.run(["cd",  "&& git add . && git commit -m \"sync\" && git push -u origin quang" % location], shell=True)
 
-with open(os.path.join(__location__, 'log\\temp.log'), 'w') as wlog:
-    subprocess.run(["cd", location, "&&", "git", "add", ".", "&&", "git", "commit", "-m", "\"sync\"", "&&", "git", "push", "-u", "origin", "quang"], stdout=wlog, stderr=wlog, shell=True)
+
+
+
+
 
 # with open(os.path.join(__location__, 'test.txt'), 'w') as wlog:
 #     wlog.write('testttttttt')
