e2700d9aa1a2eec11f40a6ffb707e47594a60798
dirii <minhquang116@gmail.com>
2020-12-31 20:18:49 +0700

    sync

--- ContestGiun/run.py
+++ ContestGiun/run.py
@@ -120,7 +120,7 @@ def run():
         wRunClient('Received Problem and Start Contest')
         print('Start Contest')
 
-    timeDo = [20, 15]
+    timeDo = [20, 35]
     try:
         while datetime.now().hour < timeDo[0] or (datetime.now().hour == timeDo[0] and datetime.now().minute <= timeDo[1]):
             if statusTasks():
