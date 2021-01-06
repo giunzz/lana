1e9c93aa7adca94fe4675c8babd772eefcdfc088
dirii <minhquang116@gmail.com>
2021-01-06 07:09:19 +0700

    sync

---
+++ JavaScript/test/test1.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require ('path');
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname + '\\views'));
+
+app.get('/', function (req, res) {
+    res.render('index')
+});
+
+app.listen(3000);
