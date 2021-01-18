007fc283454a086fdaf64ac2c8d0cf94a034d76c
dirii <minhquang116@gmail.com>
2021-01-18 16:19:24 +0700

    sync

--- Giun/BDHSG/test1.cpp
+++ Giun/BDHSG/test1.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 int main(){
-    long long a, b = 0;
-    // cin >> a;
-    while(true){
-        
+    srand((int)time(0));
+    int N = 374637, n, i = 1;
+    // cout << N << '\n';
+    while(cin >> n){
+        if(n == N) {
+            cout << "Chien thang" << endl;
+            cout << "So lan doan " << i;
+            break;
+        }
+        else{
+            if(n > N) {
+                cout << "Lon hon" << endl;
+            }
+            else {
+                cout << "Nho hon" << endl;
+            }
+        }
+        ++i;
     }
 }
---
+++ Giun/BDHSG/test2.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[100];
+    a[0] = 8
+}
