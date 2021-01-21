1e29d1971346ab0d8b882fe3dcfc7d00b4913a9d
dirii <minhquang116@gmail.com>
2021-01-21 22:05:28 +0700

    sync

--- BHuong/DIGIT0.cpp
+++ BHuong/DIGIT0.cpp
@@ -1,30 +1,49 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define giuncute ios_base::sync_with_stdio(0),cin.tie(0);
 using namespace std;
-#define giuncute ios_base::sync_with_stdio(0) , cin.tie(0);
-string s ;
-int dd[30]={0};
-ll POW (int n , int x)
+inline ll POW (int x , int n) //  x^n
 {
-    if (n == 0) return 1;
-    ll baby = POW(n/x);
-    ll ans = baby*baby;
-    if (n%x) ans *= x;
-    return ans;
+    if (n == 1) return x;
+    ll baby = POW(x,n/2);
+    if (n % 2 ) return baby*baby*x;
+    return baby*baby;
 }
-
+inline int calc (int n )
+{
+    int t = 1 , k = 0;
+    while ( POW(5,t) <= n )
+    {
+        k += n / POW(5,t);
+        t++;
+    }
+    int t1 = 1 , k1 = 0;
+    while ( POW(2,t1) <= n )
+    {
+        k1 += n / POW(2,t);
+        t1++;
+    }
+    return min(k1,k);
+}
+string  a;
+int dd[300]={0};
 int main()
 {
     giuncute;
-    freopen("giun.inp","r",stdin);
-    freopen("g.out","w",stdout);
-    cin >> s;
-    for (int i = 0 ; i < s.length() ; i++) dd[s[i]]++;
-    int n = s.size();
-    int k = 1 , uoc = 0;
-    while (n / POW(5,k) != 0)
+    freopen("digit0.inp","r",stdin);
+    freopen("digit0.out","w",stdout);
+    cin >> a;
+    int n = a.size()  ;
+    for (int i = 0 ; i < a.size() ; i++) dd[a[i]]++;
+    int count1 = calc(n) , count2 = 0;
+    for (int i = 'a' ; i <= 'z' ; i++)
     {
-        k++;
-        uoc += n / POW(5,k);
+        if (dd[i] != 0) 
+        {
+            count2 += calc(dd[i]);
+        }
     }
-    
+    if (!count2) cout << 0;
+    else cout << count1/count2;
 }
+//testtttt
