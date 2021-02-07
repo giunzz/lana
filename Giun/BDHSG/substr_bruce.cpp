c9394f8def545644cbd75c2436bb2a737acb4632
dirii <minhquang116@gmail.com>
2021-02-07 21:12:18 +0700

    sync

--- Giun/BDHSG/substr_bruce.cpp
+++ Giun/BDHSG/substr_bruce.cpp
@@ -20,10 +20,10 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
-cll MOD = 1e9 + 7, maxn = 2, maxk = 1e5 + 7;
-ll n, k, dp[maxn][maxk] = {{0}};
+ll n, k;
 
-// #define ONLINE_JUDGE
+void
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -31,29 +31,7 @@ int main(){
     OF();
     #endif
     cin >> n >> k;
-    // lp(i, 1, n) dp[i][0] = 1;
-    dp[0][0] = dp[1][0] = 1;
-    lp(i, 1, n){
-        cerr << i << '\t' << dp[i & 1][0] << ' ';
-        // (dp[i & 1][0] += dp[~i & 1][0]) %= MOD;
-        lp(j, 1, k){
-            dp[i & 1][j] = 0;
-            lp(z, max(0LL, j - i + 1), j) (dp[i & 1][j] += dp[(i - 1) & 1][z]) %= MOD;
-            // dp[i & 1][j] = dp[i & 1][j - 1] + dp[~i & 1][j];
-            // if(j - i >= 0) dp[i & 1][j] -= dp[~i & 1][j - i];
-            // dp[i & 1][j] %= MOD;
-            // if(j > i * (i - 1) / 2) dp[i & 1][j] = dp[i & 1][j - 1];
-            // (dp[i & 1][j] = dp[~i & 1][j] - (j - i >= 0 ? dp[~i & 1][j - i] : 0) + dp[i & 1][j - 1]) %= MOD;
-            cerr << dp[i & 1][j] << ' ';
-        }
-        cerr << '\n';
-    }
-    ll ans = dp[n & 1][k] - dp[n & 1][k - 1];
-    // if(k - n >= 0) ans -= dp[n][k - n];
-    // lp(i, 0, n){
-    //     cerr << i << '\t'; 
-    //     lp(j, 0, k) cerr << dp[i][j] << ' ';
-    //     cerr << '\n';
-    // }
-    cout << dp[n & 1][k];
+    if(k >= ((n * (n - 1)) >> 1))
+        cout << '0';
+    else sol();
 }
