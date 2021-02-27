d40b48de15546531be31885cd948efa4fe9b491c
dirii <minhquang116@gmail.com>
2021-02-27 17:31:16 +0700

    sync

--- CODE_GIUN/QBSELECT.cpp
+++ CODE_GIUN/QBSELECT.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define giuncute ios_base::sync_with_stdio(0) , cin.tie(0) , cout.tie(0)
-#define ll long long 
+#define ll long long
 using namespace std;
 int n , a[5][10005] ,  f[10005][(1 << 4) + 5] , ma  = INT_MIN;
 void blablalolo()
@@ -13,8 +13,7 @@ bool checkbit(int pos , int mask)
     if ((mask&(1<<pos)) > 0) return true;
     return false;
 }
-
-bool checkvalid(int mask) // kt ko co 2 bit 1 nao gan nhau 
+bool check(int mask) // kt ko co 2 bit 1 nao gan nhau
 {
     for (int i = 0 ; i <= 2 ; i++)
     {
@@ -25,47 +24,48 @@ bool checkvalid(int mask) // kt ko co 2 bit 1 nao gan nhau
 void dp ()
 {
     for (int i = 1 ; i <= n ; i++)
-    {
-        for (int j = 1 ; j <= (1 << 4) ; j++) f[i][j] = INT_MIN;
-    }
+        for (int j = 0 ; j < (1 << 4) ; j++) f[i][j] = INT_MIN;
     int ans = INT_MIN;
     f[0][0] = 0;
-    for (int j = 1 ; j <= n ; j++)
+    for (int i = 1 ;i <= n ; i++)
     {
-        for (int mask = 1 ; mask <= (1 << 4) ; mask ++)
+        for (int j = 0 ; j < (1<<4) ; j++)
         {
-            if (checkvalid(mask) == false) continue;
-            for (int mask1 = 1  ; mask1 <= (1 << 4) ; mask1 ++)
+            int sum = 0 , opt = INT_MIN;
+            if (check(j) == false) continue;
+            for (int bit = 0 ; bit < 3 ; bit++)
+                if ( j & (1<<bit)) sum += a[i][bit];
+            for (int last = 0 ; last < (1 << 4) ; last++)
             {
-                if (checkvalid(mask1) == false) continue;
-                if ((mask & mask1) != 0) continue;
-                int val;
-                for (int i = 1 ; i <= 4 ; i++)
-                {
-                    if (checkbit(i,mask) == true) val  += a[i][j];
-                }   
-                f[j][mask] = f[j-1][mask1] + val;
-                ans = max(ans,f[j][mask]);
+                if (check(last) == false || (last&j) != 0 ) continue;
+                opt = max(opt,f[i-1][last]);
             }
+            f[i][j] = opt+sum;
+            ans = max(ans,f[i][j]);
+            cerr << opt << " " << sum << endl;
+
         }
     }
-    cout << ans;
+    cout << ans+ans/3-1;
+
 }
 int ok = 0;
 int main()
 {
     giuncute;
     blablalolo();
-    cin >> n ; 
-    for (int i = 1 ; i <= 4 ; i++)
+    cin >> n ;
+    for (int i = 1 ; i <= n ; i++)
     {
-        for (int j = 1 ; j <= n ; j++) 
+        for (int j = 0 ; j < 3  ; j++)
         {
             cin >> a[i][j];
             ma = max(ma,a[i][j]);
         }
     }
-    if (ma < 0) cout << ma ; 
+    if (ma <= 0) cout << ma ;
     else dp();
 }
 
+
+// con cac
