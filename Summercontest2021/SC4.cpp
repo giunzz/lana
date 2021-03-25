50a0a79d30d223de62aff31e30c1dadab0a1c3e4
giunzz <dunghoang0401237@gmail.com>
2021-03-26 01:29:55 +0700

    sync

--- Summercontest2021/SC4.cpp
+++ Summercontest2021/SC4.cpp
@@ -1,44 +1,65 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define cll const ll
+#define pl pair<ll,ll>
 #define giuncute  ios_base::sync_with_stdio(0),cin.tie(0); cout.tie(0);
 cll maxn = 100007;
 using namespace std;
-ll n , k , c, type[maxn] ,  t  , test = 0 , need[maxn] , cost[maxn];
+ll n , k , c, type[maxn] ,  t   , cost[maxn] , test = 0;
+struct giun
+{
+    int first , second;
+};
+bool cmp (giun &a , giun &b)
+{
+    return (a.first < b.first);
+}
 int main()
 {
     giuncute;
+    giun need[100];
+    ll dd[maxn] = {0};
     freopen("giun.inp","r",stdin);
     freopen("giun.out","w",stdout);
     cin >> t;
     while (t--)
     {
         cin >> n >> k >> c;
-        for (int i = 1 ; i <= k ; i++) need[i] = INT_MAX;
-        for (int i = 1 ; i <= n ; i++) cin >> type[i];
+        test = 0;
+        for (int i = 1 ; i <= 50 ; i++) need[i].first = INT_MAX;
+        for (int i = 1 ; i <= n ; i++) cin >> type[i] , test = max(test,type[i]);
+        //cerr << test << endl;
         for (int i = 1 ; i <= n ; i++) 
         {
             cin >> cost[i] ;
-            need[type[i]] = min(need[type[i]], cost[i]);
+            if (need[type[i]].first > cost[i] ) need[type[i]].first = cost[i] , need[type[i]].second = i;
         }
+        sort(need+1,need+1+50,cmp);
         for (int i = 1 ; i <= k ; i++) 
-            if (need[i] != INT_MAX) c-= need[i];
-        cerr << c << " ";
-        if (c < 0 ) cout << "FRIENDSHIP" << endl;
+        {
+            c-= need[i].first , dd[need[i].second] = 1;
+        }
+        cout <<  1;
+        cerr << c << endl;
+        cerr << test << " " << k ;
+        if (c < 0 || test < k) cout << "FRIENDSHIP" << endl;
         else if (c == 0) cout << "NOT FRIENDSHIP BUT NOT LOVE" << endl;
         else if (c > 0)
         {
             ll ok = 0 ;
             for (int i = 1 ; i <= n ; i++)
             {
-                if (need[type[i]] != cost[i]) c -= cost[i];
-                if (cost >= 0)
+                if (dd[i] == 0) 
                 {
-                    ok = 1;
-                    cout << "LOVE" << endl ;
-                    break;
+                    c -= cost[i];
+                    if (c >= 0)
+                    {
+                        ok = 1;
+                        cout << "LOVE" << endl ;
+                        break;
+                    }
+                    else c += cost[i];
                 }
-                else c += cost[i];
             }
             if (ok == 0) cout << "NOT FRIENDSHIP BUT NOT LOVE" << endl;
         }
