e29b34a1e44ec446d7d691d057b2beb6208d75b2
giunzz <dunghoang0401237@gmail.com>
2021-04-06 22:45:27 +0700

    sync

--- CODE_GIUN/FLOYD.cpp
+++ CODE_GIUN/FLOYD.cpp
@@ -9,13 +9,13 @@ ll N,M, S , T;
 ll d[MAXN][MAXN]={0} , p[MAXN][MAXN]={0}  , dt[MAXN][MAXN]={0};
 void trace (int i , int j )
 {
-			if ( p[i][j] == 0 ) return;
-			else 
-			{
-				trace(i , p[i][j]) ;
-				cout << p[i][j] << " ";
-				trace(p[i][j],j);
-			}
+    if ( p[i][j] == 0 ) return;
+    else 
+    {
+        trace(i , p[i][j]) ;
+        cout << p[i][j] << " ";
+        trace(p[i][j],j);
+    }
 }
 void floy()
 {
@@ -29,7 +29,6 @@ void floy()
                 {
                     d[i][j] = d[i][k] + d[j][k];
                     p[i][j] = k;
-                    p[j][i] = k;
                 }
             }
         }
@@ -49,15 +48,7 @@ int main()
         dt[x][y] = l; 
         dt[y][x] = l;
     }
-    for (int u = 1 ; u <= N ; u++)
-    {
-        for (int v = 1 ; v <= N ; v++)
-        {
-            if (u == v) d[u][v] = 0;
-            else d[u][v] = (dt[u][v]) ? dt[u][v] : oo;
-            p[u][v] = (dt[u][v]) ? v : 0;
-        }
-    }
+    
     floy();
     for (int i = 1 ; i <= N ; i++)
     {
