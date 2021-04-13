11fbc005a95f1b60c3024c6e8a82a215f745949b
dirii <minhquang116@gmail.com>
2021-04-13 07:02:37 +0700

    sync

--- Giun/BDHSG/sample.cpp
+++ Giun/BDHSG/sample.cpp
@@ -36,15 +36,19 @@ void OF(){
 
 // n <= 1e5, m <= 16, k <= m;
 cll mxn = 1e5 + 7, mxm = (1 << 16) + 3;
-ll n, m, k, cnt[mxm] = {0};
+ll n, m, k, cntbit[mxm] = {0}, test = 0, ans = 0;
 vec(ll) jump[mxm];
 
-void gene(ll pos, bool ok, ll cnt, bitset<17> num, bitset<17> const t){
+inline void gene(ll pos, bool ok, ll cnt, bitset<17> &num, bitset<17> const &t){
     if(pos == 0){
-        if(cnt == k) jump[t.to_ullong()].push_back(num.to_ullong());
+        if(cnt == k){ 
+            // jump[t.to_ullong()].push_back(num.to_ullong());
+            ans += cntbit[t.to_ullong()] * cntbit[num.to_ullong()];
+            if(t == num) --ans;
+        }
     } else{
         ll npos = pos - 1;
-        bitset<17> nnum = num;
+        bitset<17> &nnum = num;
         lp(i, 0, 1){
             nnum[npos] = i;
             if(ok){
@@ -70,7 +74,8 @@ void gene(ll pos, bool ok, ll cnt, bitset<17> num, bitset<17> const t){
 
 void init(){
     lp(i, 0, mxm - 1){
-        gene(16, 1, 0, 0, i);
+        bitset<17> tmp = 0, tmp1 = i;
+        gene(16, 1, 0, tmp, tmp1);
     }
 }
 
@@ -80,13 +85,14 @@ int main(){
     OF();
     #endif
     cin >> n >> m >> k;
-    init();
     lp(i, 1, n){
         string s;
         cin >> s;
-        ++cnt[bitset<17>(s).to_ullong()];
+        ++cntbit[bitset<17>(s).to_ullong()];
     }
-    cerr << clock();
+    init();
+    cerr << test << ' ' << clock();
+    cout << ans;
     // cerr << bitset<6>(18) << '\n';
     // for(ll i : jump[18]) cerr << bitset<6>(i) << '\n';
 }
