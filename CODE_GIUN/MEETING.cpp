07911d6022beb7f6b547156e23a9dc7b33aaa082
giunzz <dunghoang0401237@gmail.com>
2021-04-27 19:18:50 +0700

    sync

--- CODE_GIUN/MEETING.cpp
+++ CODE_GIUN/MEETING.cpp
@@ -7,26 +7,6 @@ const ll maxn = 2008;
 ll dp[maxn][maxn], trace[maxn];
 ll m , n , u , v , k , t , x;
 vector<pair<ll,pair<ll,ll>>> G[maxn];
-/*  
-    dp[i][j] la thoi gian ngan nhat tu A den i mat j dong
-    q.push({dp[i][j], {i, j}});
-    *********************
-    dp[i][j] = 1e17;
-    q.push({0, {A, 0}});
-    dp[A][0] = 0;
-    while(q.size()){
-        u = q.top.second.first, costu = q.top.second.second, timeu = q.top.first;
-        q.pop();
-        if(timeu != dp[u][costu]) continue;
-        for(auto i : g[u]){
-            ll v = i.v, costv = costu + i.cost, timeuv = i.time;
-            if(costv <= k && dp[v][costv] > dp[u][costu] + timeuv){
-                dp[v][costv] = dp[u][costu] + timeuv;
-                q.push({dp[v][costv], {v, costv}});
-            }
-        }
-    }
-*/
 void DIJKSTRA (int startnode)
 {
     priority_queue< pi, vector<pair<ll,pair<ll,ll>>> , greater<pi>> pq;
@@ -44,11 +24,10 @@ void DIJKSTRA (int startnode)
             for(auto i : G[u])
             {
                 ll v = i.first, costv = costu + i.second.second, timeuv = i.second.first;
-                if(costv <= k && dp[v][costv] > dp[u][costu] + timeuv)
+                if(costv < k && dp[v][costv] > dp[u][costu] + timeuv)
                 {
                     dp[v][costv] = dp[u][costu] + timeuv;
                     pq.push({dp[v][costv], {v, costv}});
-                     trace[costv] = costu;
                 }
             }
         }
@@ -68,13 +47,8 @@ int main()
     }
     cin >> o >> p;
     DIJKSTRA(o);
-    cerr << n << endl;
-    for (int  i = 1 ; i <= n ; i++)
-    {
-        for (int j = 1 ; j <= k ; j++) cerr<< dp[i][j] << " ";
-        cerr << endl;
-    }
-    cerr << dp[4][7] ;
-    if (dp[o][p] != 1e17) cout << dp[p][7];
+    ll ans = 1e17 ;
+    for (int i = 1 ; i <= k ; i++) ans = min(dp[p][i],ans);
+    if (ans != 1e17) cout << ans;
     else cout << -1;
 }
--- CODE_GIUN/test.cpp
+++ CODE_GIUN/test.cpp
@@ -1,127 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long 
+#define pl pair<ll,ll> 
+#define giuncute ios_base::sync_with_stdio(0),cin.tie(0)
+ll n , m , s , t , dp[(int) 1e5+7];
+vector<pair<ll,ll>> G[(int)1e5+7];
 
-#define ll long long
-
-const ll mxdigit = 17;
-ll dp_sol[mxdigit][10][2], dp_cnt[mxdigit][10][2];
-vector<ll> digit;
-
-ll cnt(ll pos, ll num, bool ok){
-    if(pos == mxdigit - 1)
-        return dp_cnt[pos][num][ok] = 1; 
-    if(~dp_cnt[pos][num][ok]) return dp_cnt[pos][num][ok];
-    ll npos = pos + 1, res = 0;
-    for(ll nnum = 0; nnum <= 9; ++nnum){
-        if(ok){
-            if(nnum < digit[npos]) res += cnt(npos, nnum, 0);
-            else if(nnum == digit[npos]) res += cnt(npos, nnum, 1);
-        } else{
-            res += cnt(npos, nnum, ok);
+void dijkstra(int node)
+{
+    priority_queue< pl , vector<pl> , greater<pl>> p;
+    for (int i = 1 ; i <= n ; i++) dp[i] = 1e17;
+    p.push({0,node});
+    dp[node] = 0 ;
+    while (p.size())
+    {
+        ll u = p.top().second  , disu = p.top().first;
+        p.pop();
+        cerr << u << " " << disu << endl;
+        if (dp[u] != disu) continue;
+        for (pl v : G[u])
+        {
+            if (dp[v.first] > disu + v.second )
+            {
+                dp[v.first] = disu + v.second;
+                p.push({dp[v.first] , v.first});
+            }
         }
     }
-    return dp_cnt[pos][num][ok] = res;
-}
 
-ll sol(ll pos, ll num, bool ok){
-    if(pos == mxdigit - 1)
-        return dp_sol[pos][num][ok] = num;
-    if(~dp_sol[pos][num][ok]) return dp_sol[pos][num][ok];
-    ll npos = pos + 1, res = 0;
-    for(ll nnum = 0; nnum <= 9; ++nnum){
-        if(ok){
-            if(nnum < digit[npos]){
-                res += sol(npos, nnum, 0) + num * cnt(npos, nnum, 0);
-            } else if(nnum == digit[npos]) res += sol(npos, nnum, 1) + num * cnt(npos, nnum, 1);
-        } else{
-            res += sol(npos, nnum, ok) + num * cnt(npos, nnum, ok);
-        }
+}
+int main()
+{
+    freopen("giun.inp","r",stdin);
+    freopen("giun.out","w",stdout);
+    cin >> n >> m ;
+    for (int i = 1 ; i <= m ; i++ )
+    {
+        ll u , v , w;
+        cin >> u >> v >> w;
+        G[u].push_back({v,w});
+        G[v].push_back({u,w});
     }
-    return dp_sol[pos][num][ok] = res;
+    cin >> s >> t ;
+    cerr << s << " " << t << endl;
+    dijkstra(s);
+    for (int i = 1 ; i <= n ; i++) cerr << dp[i] << " ";
+    if (dp[t] != 1e17) cout << dp[t];
+    else cout << -1;
 }
-
-ll cal(ll n){
-    digit.clear();
-    while(n) digit.push_back(n % 10), n /= 10;
-    while(digit.size() < mxdigit) digit.push_back(0);
-    reverse(digit.begin(), digit.end());
-    memset(dp_cnt, -1, sizeof dp_cnt);
-    memset(dp_sol, -1, sizeof dp_sol);
-    return sol(0, 0, 1);
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    ll tests;
-    cin >> tests;
-    while(tests--){
-        ll l, r;
-        cin >> l >> r;
-        cout << cal(r) - cal(l - 1) << '\n';
-    }
-}
-
-/*
-#include<bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-
-const ll mxdigit = 5;
-ll n;
-vector<ll> digit;
-// *****
-// 
-
-ll cnt(ll pos, ll num, bool ok){
-    if(pos == mxdigit - 1)
-        return 1; 
-    ll npos = pos + 1, res = 0;
-    for(ll nnum = 0; nnum <= 9; ++nnum){
-        if(ok){
-            if(nnum < digit[npos]) res += cnt(npos, nnum, 0);
-            else if(nnum == digit[npos]) res += cnt(npos, nnum, 1);
-        } else{
-            res += cnt(npos, nnum, ok);
-        }
-    }
-    return res;
-}
-
-// sol(2, 4, 1) -> sol(3, 1, 0)
-// sol(2, 1, 0) -> sol(3, 1, 0)
-// ....2    5 ... = 1000 + 2 * 99 
-//    pos npos
-// ..........6
-
-ll sol(ll pos, ll num, bool ok){
-    if(pos == mxdigit - 1)
-        return num;
-    ll npos = pos + 1, res = 0;
-    for(ll nnum = 0; nnum <= 9; ++nnum){
-        if(ok){
-            // 024354
-            // 021*** 
-            if(nnum < digit[npos]){
-                res += sol(npos, nnum, 0) + num * cnt(npos, nnum, 0);
-            } else if(nnum == digit[npos]) res += sol(npos, nnum, 1) + num * cnt(npos, nnum, 1);
-        } else{
-            res += sol(npos, nnum, ok) + num * cnt(npos, nnum, ok);
-        }
-    }
-    return res;
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    freopen("f.inp", "r", stdin);
-    freopen("f.out", "w", stdout);
-    cin >> n;
-    while(n) digit.push_back(n % 10), n /= 10;
-    while(digit.size() < mxdigit) digit.push_back(0);
-    reverse(digit.begin(), digit.end());
-    cout << sol(0, 0, 1);
-}
-*/
