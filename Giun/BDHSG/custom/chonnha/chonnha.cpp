54002297141af52a5926bfc9e0d96609033a090d
DiriiMQ <minhquang116@gmail.com>
2021-05-22 20:46:41 +0700

    sync

--- Giun/BDHSG/custom/chonnha/chonnha.cpp
+++ Giun/BDHSG/custom/chonnha/chonnha.cpp
@@ -34,6 +34,59 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
+cll mxn = 1e6 + 7;
+ll n, sum[mxn] = {0}, dp_left[mxn] = {0}, dp_right[mxn] = {0};
+
+int main(){
+    giuncute();
+    OF();
+    cin >> n;
+    lp(i, 1, n) sum[i] = sum[i - 1] + read();
+    lp(i, 1, n) dp_left[i] = dp_left[i - 1] + sum[i - 1];
+    lpd(i, n, 1) dp_right[i] = dp_right[i + 1] + (sum[n] - sum[i]); //So luong nguoi i + 1 nha cuoi
+    ll res, mxsum = 1e18;
+    lp(i, 1, n)
+        if(mxsum >= dp_left[i] + dp_right[i]) mxsum = dp_left[i] + dp_right[i], res = i;
+    cout << res;
+}
+
+/*
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define EACHCASE lpd(cs, read(), 1)
+#define Fname "chonnha"
+using namespace std;
+
+template <typename T> inline void Read(T &x){
+    x = 0; char c;
+    while(!isdigit(c = getchar()));
+    do
+    {
+        x = x * 10 + c - '0';
+    } while (isdigit(c = getchar()));
+}
+
+ll read(){
+    ll tmp;
+    cin >> tmp;
+    return tmp;
+}
+
+void giuncute(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
 cll mxn = 1e7;
 ll n, a[mxn], sum = 0;
 
@@ -56,6 +109,7 @@ int main(){
         sum -= a[i]; 
     }
 }
+*/
 
 /*
 #include <bits/stdc++.h>
