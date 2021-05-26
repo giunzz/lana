b630ab219ca007ca3e42de8c0d3b1d074f98a9f7
DiriiMQ <minhquang116@gmail.com>
2021-05-26 21:05:21 +0700

    sync

--- Giun/BDHSG/aznet.cpp
+++ Giun/BDHSG/aznet.cpp
@@ -63,27 +63,30 @@ int main(){
     OF();
     #endif
     dsu d1, d2;
+    ll mxa = 0, mna = 0, mxb = 0, mnb = 0, mnsum = 1e18, cnta, cntb;
     EACHCASE{
+        mxa = 0, mna = 0, mxb = 0, mnb = 0, mnsum = 1e18;
         cin >> n >> m;
         d1.init(); d2.init();
         lp(i, 1, n - 1) cin >> a[i];
         lp(i, 1, n - 1) cin >> b[i];
         lp(i, 1, m){cin >> e[i].first.first >> e[i].first.second >> e[i].second; --e[i].second;}
-        ll mxa = 0, mna = 0, mxb = 0, mnb = 0, mnsum = 1e18, cnta, cntb;
+        
         lp(i, 1, m)
-            if(!e[i].second && d1.join(e[i].first.first, e[i].first.second)) ++mxa;
-            else if(e[i].second && d2.join(e[i].first.first, e[i].first.second)) ++mxb;
-        mna = n - 1 - mxb;
-        lp(i, mna, mxa) if(a[i] + b[n - 1 - i] < mnsum) mnsum = a[i] + b[n - 1 - i], cnta = i;
+            if(!e[i].second) {if(d1.join(e[i].first.first, e[i].first.second)) ++mxa;}
+            else if(d2.join(e[i].first.first, e[i].first.second)) ++mxb;
+        mna = n - 1 - mxb, mnb = n - 1 - mxa;
+        lp(i, mna, mxa) if(a[i] + b[n - 1 - i] < mnsum && mna <= i && i <= mxa && mnb <= n - 1 - i && n - 1 - i <= mxb) mnsum = a[i] + b[n - 1 - i], cnta = i;
         cntb = n - 1 - cnta;
         d1.init();
-        lp(i, 1, m) if(cnta && !e[i].second && d2.join(e[i].first.first, e[i].first.second)){
+        lp(i, 1, m) if(cnta && !e[i].second){
+            if(!d2.join(e[i].first.first, e[i].first.second)) continue;
             d1.join(e[i].first.first, e[i].first.second);
             cout << i << ' ';
             --cnta;
         }
-        lp(i, 1, m) if(cntb && e[i].second && d1.join(e[i].first.first, e[i].first.second)) cout << i << ' ', --cntb;
-        lp(i, 1, m) if(cnta && !e[i].second && d1.join(e[i].first.first, e[i].first.second)) cout << i << ' ', --cnta;
+        lp(i, 1, m) if(cnta && !e[i].second) if(d1.join(e[i].first.first, e[i].first.second)) cout << i << ' ', --cnta;
+        lp(i, 1, m) if(cntb && e[i].second) if(d1.join(e[i].first.first, e[i].first.second)) cout << i << ' ', --cntb;
         // cerr << cnta << ' ' << cntb << '\n';
         cout << '\n';
     }
