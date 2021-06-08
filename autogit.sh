779c529e3aa9d9f7e58c5a53ef33862f48557cd4
dirii <minhquang116@gmail.com>
2021-06-09 00:05:18 +0700

    sync

--- autogit.sh
+++ autogit.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 cd ~/Desktop/main/hoc && git pull origin quang && git add . && git commit -m "sync" && git push -u origin quang
+cd ~/Desktop/main/hoc && git pull origin quang && git add . && git commit -m "sync" && git push -u origin quang
