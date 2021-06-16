2734742fd33ff745c3dd8dbed0191eb0c419dc0b
Dirii <minhquang116@gmail.com>
2021-06-16 17:49:49 +0700

    sync

--- autogit.sh
+++ autogit.sh
@@ -2,6 +2,6 @@
 DATE=$(date +'%F %H:%M:%S')
 DIR=/home/quang/Desktop/main/hoc
 echo "Syncing: $DATE" > $DIR/autogit.log
-cd /home/quang/Desktop/main/hoc && git pull origin quang && git add . && git commit -m "sync" 
-cd /home/quang/Desktop/main/hoc && git push -u origin quang
-cd /home/quang/Desktop/main/hoc && git pull origin quang && git add . && git commit -m "sync" && git push -u origin quang
+cd /home/quang/main/hoc && git pull origin quang && git add . && git commit -m "sync" 
+cd /home/quang/main/hoc && git push -u origin quang
+cd /home/quang/main/hoc && git pull origin quang && git add . && git commit -m "sync" && git push -u origin quang
