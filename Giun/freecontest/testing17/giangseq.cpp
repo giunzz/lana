2b56fd6e9354a33cdd4edf1c020d755a448422cd
Dirii <minhquang116@gmail.com>
2021-06-20 12:31:27 +0700

    sync

---
+++ Giun/freecontest/testing17/giangseq.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define EACHCASE lpd(cs, read(), 1)
+#define Fname "f"
+using namespace std;
+
+template <typename T> inline void Read(T &x){
+    x = 0; char c;
+    while(!isdigit(c = getchar()));
+    do
+    {
+        x = x * 10 + c - '0';
+    } while (isdigit(c = getchar()));
+}
+
+ll read(){
+    ll tmp;
+    cin >> tmp;
+    return tmp;
+}
+
+void giuncute(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+cll mxn = 2e3 + 7;
+ll n, a[mxn];
+pp(ll, ll) b[mxn];
+
+int main(){
+    giuncute();
+    #ifndef ONLINE_JUDGE
+    OF();
+    #endif
+    cin >> n;
+    lp(i, 1, n) cin >> a[i];
+    b[1] = {abs(a[1] - a[2]), 1};
+    b[n] = {abs(a[n] - a[n - 1]), n};
+    lp(i, 2, n - 1) b[i] = {max(abs(a[i] - a[i - 1]), abs(a[i] - a[i + 1])), i};
+    sort(b + 1, b + 1 + n, greater<pp(ll, ll)>());
+    
+}
