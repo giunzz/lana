4ea89eb921eb9f24ac555f70a430e36ff8d70b44
Dirii <minhquang116@gmail.com>
2021-06-24 21:35:12 +0700

    sync

--- Giun/vnoj/practice/move12.cpp
+++ Giun/vnoj/practice/move12.cpp
@@ -34,10 +34,31 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
+cll mxn = 1e4 + 7;
+ll n, c[mxn], t[mxn], l[mxn], r[mxn];
+
+inline bool check(ll time){
+    lp(i, 1, n) l[i] = r[i] = 0;
+    lp(i, 1, n) ++l[max(1LL, c[i] - time / t[i])], ++r[min(n, c[i] + time / t[i])];
+    lp(i, 1, n){
+        l[i] += l[i - 1], r[i] += r[i - 1];
+        if(l[i] < i or r[i] > i) return 0;
+    }
+    return 1;
+}
+
 int main(){
     giuncute();
     #ifndef ONLINE_JUDGE
     OF();
     #endif
-    
+    cin >> n;
+    lp(i, 1, n) cin >> c[i] >> t[i];
+    ll l = 0, r = 1e9, ans;
+    while(l <= r){
+        ll mid = (l + r) >> 1;
+        if(check(mid)) ans = mid, r = mid - 1;
+        else l = mid + 1;
+    }
+    cout << ans;
 }
