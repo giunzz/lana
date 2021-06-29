a870c1e1abe3703979a803a06a979a74d451c50c
Dirii <minhquang116@gmail.com>
2021-06-29 19:49:41 +0700

    sync

--- Python/checker/killFileUnix.py
+++ Python/checker/killFileUnix.py
@@ -1,11 +1,12 @@
 import os
+from subprocess import check_output
 
 def getpid(process_name):
-    return os.system('pgrep -n ' + process_name)
+    pid = [int(i) for i in check_output(['pgrep', '-n', process_name]).split() if i.isdigit()]
+    return str(pid[0])
 
 def killFile(name):
-    print('kill -9 ' + str(getpid(name)))
-    os.system('kill -9 ' + str(getpid(name)))
+    os.system('kill -9 ' + getpid(name))
 
 if __name__ == "__main__":
     killFile('test1')
