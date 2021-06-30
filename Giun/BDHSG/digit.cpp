767dcdb1b77117381367aeec32e3a56a29ab18a5
DiriiMQ <minhquang116@gmail.com>
2021-06-30 09:29:18 +0700

    sync

--- Giun/BDHSG/digit.cpp
+++ Giun/BDHSG/digit.cpp
@@ -34,8 +34,8 @@ void OF(){
     freopen(Fname".out", "w", stdout);
 }
 
-cll mxx = 7e3;
-ll x, a, b, dp[2][2][13][mxx][10];
+cll mxx = 7e4;
+ll x, a, b, dp[2][2][13][mxx];
 bool al[10] = {0};
 vec(ll) dg, dg_n;
 string s;
@@ -45,13 +45,13 @@ void inp(){
     for(char i : s) dg.push_back(i - '0'), al[i - '0'] = 1;
 }
 
-ll sol_dp(bool equal, bool first0, int pos, ll div, int dig){
-    ll &cur = dp[equal][first0][pos][div][dig], npos = pos - 1, div10 = (div * 10) % x;
+ll sol_dp(bool equal, bool first0, int pos, ll div){
+    ll &cur = dp[equal][first0][pos][div], npos = pos - 1, div10 = (div * 10) % x;
     bool nequal = equal;
     if(~cur) return cur;
     if(!pos) return cur = ((!div && !first0) || (first0 && al[0]));
     cur = 0;
-    if(first0) cur += sol_dp(0, first0, npos, 0, 0);
+    if(first0) cur += sol_dp(0, first0, npos, 0);
     for(int ndig : dg){
         if(first0 && !ndig) continue;
         if(equal){
@@ -59,7 +59,7 @@ ll sol_dp(bool equal, bool first0, int pos, ll div, int dig){
             if(ndig < dg_n[npos]) nequal = 0;
             else nequal = 1;
         }
-        cur += sol_dp(nequal, 0, npos, (div10 + ndig) % x, ndig);
+        cur += sol_dp(nequal, 0, npos, (div10 + ndig) % x);
     }   
     return cur;
 }
@@ -70,7 +70,7 @@ ll pre_dp(ll num){
     reverse(s.begin(), s.end());
     for(char i : s) dg_n.push_back(i - '0');
     memset(dp, -1, sizeof dp);
-    return sol_dp(1, 1, dg_n.size(), 0, 0);
+    return sol_dp(1, 1, dg_n.size(), 0);
 }
 
 void case_dp(){
@@ -94,7 +94,7 @@ void case_bruce(){
 
 int main(){
     giuncute();
-    #ifndef
+    #ifndef ONLINE_JUDGE
     OF();
     #endif
     cin >> x >> a >> b;
