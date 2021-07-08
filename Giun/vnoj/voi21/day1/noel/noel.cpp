1ba9cbbb4417fa80f432a16f1cba74d713fe9bc0
DiriiMQ <minhquang116@gmail.com>
2021-07-08 11:11:33 +0700

    sync

---
+++ Giun/vnoj/voi21/day1/noel/noel.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "noel"
+
+cll mxn = 107;
+ll n, d, a[mxn];
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+    cin >> n >> d;
+    lp(i, 1, n) cin >> a[i];
+
+}
---
+++ Giun/vnoj/voi21/pb/pb.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; a++)
+#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define Fname "pb"
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+    
+}
