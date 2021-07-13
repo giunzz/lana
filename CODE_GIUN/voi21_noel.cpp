e9972458ef83ee42227f4d23834fff73132de29e
giunzz <dunghoang0401237@gmail.com>
2021-07-13 08:41:05 +0700

    sync

--- CODE_GIUN/voi21_noel.cpp
+++ CODE_GIUN/voi21_noel.cpp
@@ -2,25 +2,46 @@
 #define ll long long
 #define giuncute ios_base::sync_with_stdio(0),cin.tie(0);
 const ll maxn = 3e3 +  7;
-ll n , a[maxn] , ans = 0 , k , d[maxn]={0}  ;
+ll n , a[maxn] , ans = 0 , k , c[maxn]={0}  ;
 using namespace std;
 
-void check (ll x )
+bool check(ll x , vector<ll> y)
 {
-    cerr << x << endl;
-    ll m = 0 ;
-    for (int i = 1 ; i <= n ; i++ )
+    ll t = 0;
+    for (int i = 0 ; i < (int)y.size() ; i++)
     {
-        if (i + x > n) {break;}
-        if ( abs(a[i] - a[i + x]) <= k ) m++;
+        if (i+x > (int) y.size()) continue;
+        if (abs(y[i] - y[i+x]) <= k) t++;
     }
-    if (m == x) 
+    if (t == x) return true;
+    else return false;
+}
+
+void quaylui (ll vt )
+{
+    if (vt == n + 1) 
     {
-        cout << x ;
-        return ;
+        vector<ll> tmp;
+        for (int i = 1 ; i <= n ; i++)
+            if (c[i] == 1) tmp.push_back(a[i]);
+        if (tmp.size() % 2 == 0)
+        {
+            ll len = tmp.size()/2 ;
+            for (ll i = 1 ; i <= len+1 ; i++)
+            {
+                if (ans > i) continue;
+                if (check(i,tmp) == true) ans = max(ans,i);
+            }
+        }
     }
-    x--;
-    //else check(x);
+    else
+    {
+        for (int i = 0 ; i <= 1 ; i++)
+        {
+            c[vt] = i ;
+            quaylui(vt+1);
+        }
+    }
 }
 int main()
 {
@@ -28,9 +49,8 @@ int main()
     freopen("giun.inp" , "r",stdin);
     freopen("giun.out", "w" ,stdout);
     cin >> n  >> k;
-    ll ans = n ;
-    if (ans )
-    n = 2 * n ;
+    n = n*2;
     for (int i = 1 ; i <= n ; i++)  cin >> a[i];
-    check(ans);
+    quaylui(1);
+    cout << ans;
 }
