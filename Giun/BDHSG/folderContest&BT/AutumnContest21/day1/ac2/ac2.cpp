500e5b3a0b65bb1249264f0d2fc4deea39bb193c
Dirii <minhquang116@gmail.com>
2021-07-25 08:35:19 +0700

    sync

--- Giun/BDHSG/folderContest&BT/AutumnContest21/day1/ac2/ac2.cpp
+++ Giun/BDHSG/folderContest&BT/AutumnContest21/day1/ac2/ac2.cpp
@@ -1,64 +1,96 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long
 #define cll const ll
-#define lp(a, b, c) for(ll a = b; a <= c; a++)
-#define lpd(a, b, c) for(ll a = b; a >= c; a--)
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
 #define vec(a) vector<a>
 #define pp(a, b) pair<a, b>
+#define EACHCASE lpd(cs, read(), 1)
 #define Fname "ac2"
+using namespace std;
+
+template <typename T> inline void Read(T &x){
+    x = 0; char c;
+    while(!isdigit(c = getchar()));
+    do
+    {
+        x = x * 10 + c - '0';
+    } while (isdigit(c = getchar()));
+}
 
 ll read(){
     ll tmp;
     cin >> tmp;
     return tmp;
 }
-#define EACHCASE lpd(cs, read(), 1)
 
-cll mxn = 40, MOD = 998244353;
-ll n, d, dp[mxn][4][1 << 4][1 << 4];
-bool sing[mxn], dan[mxn], t[3][mxn];
+void giuncute(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
 
-void sol(ll pos, ll team, bool op, ll mask){
-    if(pos == n + 1) return;
-    ll npos = 
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+cll mxn = 38, MOD = 998244353;
+ll n, d, dp[mxn][mxn][mxn][1 << 3];
+bool a[3][mxn];
+
+void sol(){
+    memset(a, 0, sizeof a);
+    memset(dp, 0, sizeof dp);
+    cin >> n >> d;
+    EACHCASE a[1][read()] = 1;
+    EACHCASE a[2][read()] = 1;
+    lp(i, 1, n) a[0][i] = 1;
+    dp[0][0][0][0] = 1;
+    ll ans = 0;
+    lp(i, 0, n) lp(j, 0, n) lp(k, 0, n) lp(mask, 0, (1 << 3) - 1){
+        if(!dp[i][j][k][mask]) continue;
+        ll next_pos = max({i, j, k}) + 1;
+        if(next_pos > n){ 
+            if(!mask && j && k) (ans += dp[i][j][k][mask]) %= MOD;
+            continue;
+        }
+        lp(t, 0, 2) if(a[t][next_pos]){
+            if(((1 << t) & mask)){
+                if(t == 0){
+                    if(next_pos - i > d) continue;
+                    else (dp[next_pos][j][k][mask - 1] += dp[i][j][k][mask]) %= MOD;
+                }
+                if(t == 1){
+                    if(next_pos - j > d) continue;
+                    else (dp[i][next_pos][k][mask - 2] += dp[i][j][k][mask]) %= MOD;
+                }
+                if(t == 2){
+                    if(next_pos - k > d) continue;
+                    else (dp[i][j][next_pos][mask - 4] += dp[i][j][k][mask]) %= MOD;
+                }
+            }else{
+                if(t == 0){
+                    if(next_pos - i <= d && i) (dp[next_pos][j][k][mask] += dp[i][j][k][mask]) %= MOD;
+                    else (dp[next_pos][j][k][mask | 1] += dp[i][j][k][mask]) %= MOD;
+                }
+                if(t == 1){
+                    if(next_pos - j <= d && j) (dp[i][next_pos][k][mask] += dp[i][j][k][mask]) %= MOD;
+                    else (dp[i][next_pos][k][mask | 2] += dp[i][j][k][mask]) %= MOD;
+                }
+                if(t == 2){
+                    if(next_pos - k <= d && k) (dp[i][j][next_pos][mask] += dp[i][j][k][mask]) %= MOD;
+                    else (dp[i][j][next_pos][mask | 4] += dp[i][j][k][mask]) %= MOD;
+                }
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    freopen(Fname".inp", "r", stdin);
-    freopen(Fname".out", "w", stdout);
-    EACHCASE{
-        memset(dp, 0, sizeof dp);
-        cin >> n >> d;
-        ll tmp;
-        cin >> tmp;
-        lp(i, 1, tmp) t[1][read()] = 1;
-        cin >> tmp;
-        lp(i, 1, tmp) t[2]dan[read()] = 1;
-        lp(i, 1, n) t[0][i] = 1;
-        if(sing[1]) dp[1][1][1][1] = 1, sol(1, 1, 1, 1);
-        if(dan[1]) dp[1][2][1][1 << 2] = 1, sol(1, 2, 1, 1 << 2);
-        dp[1][0][1][0] = 1; sol(1, 0, 1, 0);
-
-
-        // lp(i, 1, n){
-        //     lp(team, 0, 2){
-        //         lp(mask_open, 0, (1 << 4) - 1){
-        //             lp(mask_tot, 0, (1 << 4) - 1){
-        //                 if(dp[i][team][mask_open][mask_tot]){
-        //                     if((1 << team) & mask_open){
-        //                         lp(j, 1, d){
-        //                             ll nmask_open = mask_open, nmask_tot = mask_tot;
-
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        
-    }
+    giuncute();
+    #ifndef ONLINE_JUDGE
+    OF();
+    #endif
+    EACHCASE sol();
 }
