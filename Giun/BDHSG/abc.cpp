d1f7447721e7d0ae65c93f7944c4a902de7ae821
DiriiMQ <minhquang116@gmail.com>
2021-08-01 23:24:42 +0700

    sync

---
+++ Giun/BDHSG/abc.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define cll const ll
+#define lp(a, b, c) for(ll a = b; a <= c; ++a)
+#define lpd(a, b, c) for(ll a = b; a >= c; --a)
+#define vec(a) vector<a>
+#define pp(a, b) pair<a, b>
+#define EACHCASE lpd(cs, read(), 1)
+#define Fname "f"
+using namespace std;
+
+template <typename T> inline void Read(T &x){
+    x = 0; char c;
+    while(!isdigit(c = getchar()));
+    do
+    {
+        x = x * 10 + c - '0';
+    } while (isdigit(c = getchar()));
+}
+
+ll read(){
+    ll tmp;
+    cin >> tmp;
+    return tmp;
+}
+
+void giuncute(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+void OF(){
+    freopen(Fname".inp", "r", stdin);
+    freopen(Fname".out", "w", stdout);
+}
+
+#define point pp(ll, ll)
+
+int ccw(point &a, point &b, point &c){
+    ll tmp = (b.first - a.first) * (c.second - b.second) - (b.second - a.second) * (c.first - b.first);
+     
+}
+
+point a, b, c;
+
+void input(point &x){cin >> x.first >> x.second;}
+
+
+int main(){
+    giuncute();
+    #ifndef ONLINE_JUDGE
+    OF();
+    #endif
+    EACHCASE{
+        input(a), input(b), input(c);
+
+    }
+}
--- Giun/BDHSG/kem.cpp
+++ Giun/BDHSG/kem.cpp
@@ -1,5 +1,6 @@
-#define _total 20
+#define _total 50
 #define _seed 38832
+#define _require_skip 5
 
 #include <bits/stdc++.h>
 #include <unistd.h>
@@ -15,21 +16,42 @@ ll randll(ll a, ll b){
     return uniform_int_distribution<ll>(a, b)(rng);
 }
 
+void init(){
+    cout << "Chao mung Kem lam toan\n";
+    cout << "=========================================\n";
+}
+
 bool gen(ll counter){
     system("clear");
+    init();
     cout << "Day la cau hoi thu " << counter << '\n';
-    ll phep = randll(0, 1);
-    if(phep){ // +
+    ll phep = randll(0, 3);
+    if(phep == 0){ // +
         ll num1 = randll(1, 1e5), num2 = randll(1, 1e5), ans = num1 + num2, res;
         cout << num1 << " + " << num2 << " = ";
         cin >> res;
         if(res == ans) return 1;
         else return 0;
-    } else{
+    } else if(phep == 1){
         ll num1 = randll(1, 1e5), num2 = randll(1, num1), ans = num1 - num2, res;
         cout << num1 << " - " << num2 << " = ";
         cin >> res;
         if(res == ans) return 1;
+        else return 0;
+    } else if(phep == 2){
+        ll num1 = randll(0, 1000), num2 = randll(0, 9), ans = num1 * num2, res;
+        cout << num1 << " * " << num2 << " = ";
+        cin >> res;
+        if(res == ans) return 1;
+        else return 0;
+    } else{
+        ll num1 = randll(0, 1000), num2 = randll(1, 9), ans_thuong = num1 / num2, ans_du = num1 % num2, res_thuong, res_du;
+        cout << num1 << " / " << num2 << "\n";
+        cout << "Thuong = ";
+        cin >> res_thuong;
+        cout << "Du = ";
+        cin >> res_du;
+        if(res_thuong == ans_thuong && res_du == ans_du) return 1;
         else return 0;
     }
 }
@@ -40,13 +62,15 @@ void cong(ll wa){
 }
 
 int main(){
-    ll wa = 0;
+    ll wa = 0, total_ac = 0;
     lp(i, 1, _total){
+        bool ok = 1;
         while(!gen(i)){
-            ++wa;
+            ++wa, ok = 0, total_ac = 0;
             cout << "Saiiii\n";
             usleep(1000000);
         }
+        if(++total_ac == _require_skip) i += 2;
         cout << "Dunggg\n";
         usleep(1000000);
     }
