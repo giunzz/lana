12848818c650712eaccf63e081a198032a4d8684
Dirii <minhquang116@gmail.com>
2021-08-08 19:49:23 +0700

    sync

--- Giun/atcoder/abc212_f.cpp
+++ Giun/atcoder/abc212_f.cpp
@@ -40,14 +40,25 @@ struct adj_bus{
         cin >> a >> b >> s >> t;
         id = _id;
     }
+    bool operator<(const ll &_t){
+        return s < _t;
+    }
 };
 
 cll mxn = 1e5 + 7;
-ll n, m, q;
+ll n, m, q, go[mxn] = {0};
+pp(ll, ll) ask_bus[mxn];
 adj_bus bus[mxn];
-vec(ll) bus_at[mxn];
+vec(ll) g[mxn];
+vec(adj_bus) bus_at[mxn];
 
-bool cpr(adj_bus &a, adj_bus &b){return a.t > b.t;}
+bool cpr(adj_bus &a, adj_bus &b){return a.s < b.s;}
+
+bool cpr_id(adj_bus &a, adj_bus &b){return a.id < b.id;}
+
+priority_queue dfs(ll u){
+    
+}
 
 int main(){
     giuncute();
@@ -58,8 +69,34 @@ int main(){
     lp(i, 1, m) 
         bus[i].init(i);
     sort(bus + 1, bus + 1 + m, cpr);
-    lp(i, 1, m) bus_at[bus[i].b].push_back(i);
+    lp(i, 1, m) bus_at[bus[i].a].push_back(bus[i]);
     lp(i, 1, m){
         // xu ly tao do thi + danh dau adj da them vao
+        ll b = bus[i].b, t = bus[i].t, 
+            pos = lower_bound(bus_at[b].begin(), bus_at[b].end(), t) - bus_at[b].begin();
+        if(pos != bus_at[b].size()) go[bus[i].id] = bus_at[b][pos].id;
     }
+    lp(i, 1, q){
+        ll time, city, req;
+        cin >> time >> city >> req;
+        auto pos = lower_bound(bus_at[city].begin(), bus_at[city].end(), req);
+        if(pos != bus_at[city].end()) ask_bus[(*pos).id] = {req, i};
+    }
+    lp(i, 1, n) if(go[i]) g[go[i]].push_back(i);
+    sort(bus + 1, bus + 1 + m, cpr_id);
+    lp(i, 1, n) if(!go[i]){
+        set<ll> s = dfs(i);
+    }
+    // lp(i, 1, n){
+    //     cerr << i << "\n----> ";
+    //     for(ll j : g[i]) cerr << j << ' ';
+    //     cerr << '\n';
+    // }
+    // lp(i, 1, m){
+    //     cerr << i << '\n';
+    //     for(auto j : bus_at[i]){
+    //         cerr << j.s << ' ';
+    //     }
+    //     cerr << '\n';
+    // }
 }
