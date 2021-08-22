08dd2165614a53877ec490f01f91807b0fd978a9
Dirii <minhquang116@gmail.com>
2021-08-22 17:37:14 +0700

    sync

--- Giun/vnoj/practice/fct018_fliprow.cpp
+++ Giun/vnoj/practice/fct018_fliprow.cpp
@@ -36,8 +36,30 @@ void OF(){
 
 cll mxn = 1e3 + 3;
 ll n, m, a[mxn][mxn], color[mxn];
-map<ll, ll> num[2];  
+map<ll, ll> num[2];
+bool added[mxn][mxn][2] = {{{0}}}; 
 vec(pp(ll, ll)) g[mxn];
+
+bool dfs(ll u, ll col){
+    bool ok = 1;
+    for(pp(ll, ll) i : g[u]){
+        ll v = i.first;
+        bool need = i.second, ncol;
+        if(color[v] == -1){
+            if(need) ncol = !col;
+            else ncol = col;
+            color[v] = ncol;
+            if(!dfs(v, ncol)) ok = 0;
+        } else{
+            if(need){
+                if(color[v] == color[u]) ok = 0;
+            } else{
+                if(color[v] != color[u]) ok = 0;
+            }
+        } 
+    }
+    return ok;
+}
 
 int main(){
     giuncute();
@@ -50,9 +72,9 @@ int main(){
         vec(ll) num;
         lp(i, 1, n) num.push_back(a[i][m / 2 + 1]);
         sort(num.begin(), num.end());
-        lp(i, 2, num.size() - 2){
-            if(num[i] == num[i - 1] && num[i] == num[i + 1]){
-                cout << "NO\n";
+        lp(i, 1, num.size() - 1){
+            if(num[i] == num[i - 1]){
+                cout << "No\n";
                 return 0;
             }
         }
@@ -63,12 +85,12 @@ int main(){
         ll fj = m - j + 1;
         lp(i, 1, n){
             if(a[i][j] == a[i][fj]){
-                if(num[0][a[i][j]] || num[1][a[i][fj]]){cout << "NO\n"; return 0;}
+                if(num[0][a[i][j]] || num[1][a[i][fj]]){cout << "No\n"; return 0;}
                 num[0][a[i][j]] = num[1][a[i][fj]] = -1;
                 continue;
             }
 
-            if(num[1][a[i][j]] == -1 || num[0][a[i][j]] == -1){cout << "NO\n"; return 0;}
+            if(num[1][a[i][j]] == -1 || num[0][a[i][j]] == -1){cout << "No\n"; return 0;}
             assert(!(num[1][a[i][j]] > 0 && num[0][a[i][j]] > 0));
             if(num[0][a[i][j]]){
                 g[num[0][a[i][j]]].push_back({i, 1});
@@ -80,7 +102,7 @@ int main(){
                 num[0][a[i][j]] = num[1][a[i][j]] = -1;
             } else num[0][a[i][j]] = i;
 
-            if(num[1][a[i][fj]] == -1 || num[0][a[i][fj]] == -1){cout << "NO\n"; return 0;}
+            if(num[1][a[i][fj]] == -1 || num[0][a[i][fj]] == -1){cout << "No\n"; return 0;}
             assert(!(num[1][a[i][j]] > 0 && num[0][a[i][j]] > 0));
             if(num[0][a[i][fj]]){
                 g[num[0][a[i][fj]]].push_back({i, 0});
@@ -93,9 +115,22 @@ int main(){
             } else num[1][a[i][fj]] = i;
         }
     }
+
+    // color[1] = 1;
+    bool ans = 1;
+    lp(i, 1, n) if(color[i] == -1){
+        color[i] = 1;
+        if(!dfs(i, 1)) ans = 0;
+    }
+    if(ans){
+        cout << "Yes\n" << count(color + 1, color + 1 + n, 1) << '\n';
+        lp(i, 1, n) if(color[i] == 1) cout << i << ' ';
+    } else{
+        cout << "No\n";
+    }
+    // lp(i, 1, n) cerr << i << ' ' << color[i] << '\n';
     // lp(i, 1, n){
-    //     cerr << i << "\n";
-    //     for(auto j : g[i]) cerr << '\t' << j.first << ' ' << j.second << '\n';
+    //     for(auto j : g[i]) if(i < j.first) cerr << i << ' ' << j.first << ' ' << j.second << '\n';
     // }
     
 }
--- Giun/vnoj/practice/test.cpp
+++ Giun/vnoj/practice/test.cpp
@@ -1,44 +1,78 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define cll const ll
-#define lp(a, b, c) for(ll a = b; a <= c; ++a)
-#define lpd(a, b, c) for(ll a = b; a >= c; --a)
-#define vec(a) vector<a>
-#define pp(a, b) pair<a, b>
-#define EACHCASE lpd(cs, read(), 1)
-#define Fname "f"
+#define giuncute ios_base::sync_with_stdio(0) , cin.tie(0);
+#define pd pair<double,double>
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll maxn = 1e3 + 7;
 using namespace std;
 
-template <typename T> inline void Read(T &x){
-    x = 0; char c;
-    while(!isdigit(c = getchar()));
-    do
+ll t , n , m , x ,q , u , v;
+ll size[maxn] , up[maxn][25] = {0};
+vector<ll> child[maxn];
+
+void dfs (ll u , ll father)
+{
+    up[u][0] = father ;
+    for (int j = 1 ; j < 19 ; j++)
+        up[u][j] = up[up[u][j-1]][j-1];
+    for (ll v : child[u])
     {
-        x = x * 10 + c - '0';
-    } while (isdigit(c = getchar()));
+        if (v == father) continue;
+        size[v] = size[u]  + 1 ;
+        dfs(v,u);
+    }
 }
 
-ll read(){
-    ll tmp;
-    cin >> tmp;
-    return tmp;
+ll lca (ll u , ll v)
+{
+    if (size[v] > size[u]) swap(u,v);
+    ll k = size[u] - size[v];
+    for (ll j = 18 ; j >= 0 ; j-- )
+    {
+        if (k & (1 << j)) u = up[u][j];
+    }
+    if (u == v) return u;
+    for(int j = 18; j >= 0; j--) 
+    {
+		if(up[u][j] != up[v][j]) 
+        {
+			u = up[u][j];
+			v = up[v][j];
+		}
+	}
+	return up[u][0];
 }
-
-void giuncute(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-}
-
-void OF(){
-    freopen(Fname".inp", "r", stdin);
-    freopen(Fname".out", "w", stdout);
-}
-
-int main(){
-    giuncute();
-    #ifndef ONLINE_JUDGE
-    OF();
-    #endif
-    ll a = 7, b = a--;
-    assert(a != 7);
+int main()
+{
+    giuncute;
+    freopen("f.inp","r",stdin);
+    freopen("f.out","w",stdout);
+    cin >> t;
+    // cerr << t;
+    for (int k = 1 ; k <= t ; k ++)
+    {
+        cout << "Case " << k << ":" << endl;
+        cin >> n;
+        for (int i = 1 ; i <= n ; i++)
+            for (int j = 1 ; j <= 18 ; j++) up[i][j] = 0;
+        for (int i = 1 ; i <= n ; i++)
+        {
+            size[i] = 0 ;
+            cin >> m ;
+            while (m--)
+            {
+                cin >> x ;
+                child[i].push_back(x);
+            }
+        }
+        dfs(1 , (ll) -1);
+        cin >> q;
+        while (q--)
+        {
+            cin >> u >> v;
+            cout << lca(u,v) << endl;
+        }
+    }
 }
